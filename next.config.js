41dd6ea32a3217fdf7e5798ffdb356ae4ca6c36a
ingong <insong.lee@webtoonscorp.com>
2022-10-20 01:44:00 +0900

    config: @svgr/webpack 설치 및 적용

--- next.config.js
+++ next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
   images: {
     domains: ['sopt-makers.s3.ap-northeast-2.amazonaws.com'],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+      options: {
+        svgo: false,
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
