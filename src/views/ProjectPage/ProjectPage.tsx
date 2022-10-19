5418d9e82bcfb1f6a053f6d2b9a3463fd85e92ba
ingong <insong.lee@webtoonscorp.com>
2022-10-19 11:49:36 +0900

    feat: ProjectList 컴포넌트 내 목데이터 패칭 적용

--- src/views/ProjectPage/ProjectPage.tsx
+++ src/views/ProjectPage/ProjectPage.tsx
@@ -1,14 +1,42 @@
+import { useEffect, useReducer } from 'react';
+import { to } from 'await-to-js';
+import { useMediaQuery } from 'react-responsive';
+
 import Sopt404 from '@src/assets/images/sopt_404.png';
 import Header from '@src/components/common/Header';
 import Image from 'next/image';
-import { useMediaQuery } from 'react-responsive';
+import { Condition } from '@src/lib';
 
+import { reducer } from './reducer';
 import { Styled } from './styles';
+import { getProjectList } from './mock';
+import { ProjectType } from './types';
 
 function Projects() {
-  const isDesktop = useMediaQuery({
-    query: '(min-width: 768px)',
-  });
+  const isDesktop = useMediaQuery({ query: '(min-width: 768px)' });
+  const [state, dispatch] = useReducer(reducer, { _TAG: 'IDLE' });
+  const fetchProjectList = async () => {
+    dispatch({
+      _TAG: 'FETCH',
+    });
+
+    const [error, response] = await to(getProjectList());
+
+    if (error) {
+      return dispatch({
+        _TAG: 'FAILED',
+        error,
+      });
+    }
+
+    if (response) {
+      return dispatch({ _TAG: 'SUCCESS', data: response });
+    }
+  };
+
+  useEffect(() => {
+    fetchProjectList();
+  }, []);
 
   return (
     <>
@@ -22,9 +50,41 @@ function Projects() {
           blurDataURL={Sopt404.src}
           placeholder="blur"
         />
-        <span>앗 아직 준비 중인 기능이에요!</span>
       </Styled.Root>
+      <div>
+        <section>
+          {(() => {
+            switch (state._TAG) {
+              case 'IDLE':
+                return <p>IDLE</p>;
+              case 'LOADING':
+                return <p>LOADING</p>;
+              case 'ERROR':
+                return <p>ERROR</p>;
+              case 'OK':
+                return <ProjectList list={state.data} />;
+            }
+          })()}
+        </section>
+      </div>
     </>
+  );
+}
+
+function ProjectList({ list }: { list: ProjectType[] }) {
+  const listLength = list.length;
+
+  return (
+    <div>
+      <Condition statement={listLength > 0}>
+        {list?.map((project) => (
+          <article key={project.id}>{project.name}</article>
+        ))}
+      </Condition>
+      <Condition statement={listLength === 0}>
+        <p>Project Empty</p>
+      </Condition>
+    </div>
   );
 }
 
