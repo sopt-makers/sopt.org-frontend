b84f3378f0b84a5cbbe021ecbb362102f6047e68
ingong <insong.lee@webtoonscorp.com>
2022-10-22 21:23:22 +0900

    feat: project enroll section 마크업 수정

--- src/views/ProjectPage/ProjectPage.tsx
+++ src/views/ProjectPage/ProjectPage.tsx
@@ -139,12 +139,12 @@ function Project({ project }: { project: ProjectType }) {
 
 function ProjectEnrollSection() {
   return (
-    <section className={cc([styles.enroll])} onClick={() => alert('인터널 프로젝트와 연동 예정')}>
+    <section className={styles.enroll}>
       <p>혹시 SOPT 회원으로 진행한 프로젝트가 있다면?</p>
-      <p>
+      <div onClick={() => alert('인터널 프로젝트와 연동 예정')}>
         <span>프로젝트 올리러 가기</span>
         <RightArrow />
-      </p>
+      </div>
     </section>
   );
 }
