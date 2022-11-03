a771344d1a163ca946881e8ced0def8df8bb0b05
ingong <insong.lee@webtoonscorp.com>
2022-11-03 20:44:42 +0900

    feat: ProjectType 수정

--- src/views/ProjectPage/types.ts
+++ src/views/ProjectPage/types.ts
@@ -1,25 +1,30 @@
 export interface ProjectType {
-  id: number;
-  name: string;
-  semester: number;
-  isProvidingService: boolean;
-  isBusinessing: boolean;
-  teamMembers: string[];
-  afterJoinedTeamMembers: string[];
-  serviceType: string[];
-  startDate: string;
-  endDate: string;
-  inProgress: boolean;
-  shortIntroduction: string;
+  category: CategoryType;
   detail: string;
-  logoImageUrl: string;
-  thumbnailImageUrl: string;
-  projectImageUrl: string;
+  endAt: string;
+  generation: number;
+  id: number;
+  isAvailable: boolean;
+  isFounding: boolean;
   link: LinkType[];
+  logoImage: string;
+  members: { name: string; role: string; description: string }[];
+  name: string;
+  projectImage: null;
+  serviceType: string[];
+  startAt: string;
+  summary: string;
+  thumbnailImage: null | string;
+  updatedAt: string;
+  uploadedAt: string;
 }
 
+/* category type이 추가되면 반영되어야함 */
+interface CategoryType {
+  project: string;
+}
 interface LinkType {
-  type: string; // should be changed to specific linkType
+  title: string;
   url: string;
 }
 
