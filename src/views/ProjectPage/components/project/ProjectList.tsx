cce39c3bf0f5aae799b2fb91986ff787372809a9
ingong <insong.lee@webtoonscorp.com>
2022-11-13 17:39:38 +0900

    feat: project list style 반응형 스타일 적용

--- src/views/ProjectPage/components/project/ProjectList.tsx
+++ src/views/ProjectPage/components/project/ProjectList.tsx
@@ -113,17 +113,17 @@ function ProjectCategoryDescription(category: ProjectCategoryType) {
                 <p>5주간의 집중 협업을 통해 </p>
                 <p>
                   <span>한 개의 온전한 프로덕트를 만드는 프로젝트, </span>
-                  <span>APPJAM</span>
+                  <span className={styles['category-summary']}>APPJAM</span>
                 </p>
               </div>
             );
           case ProjectCategoryType.SOPKATHON:
             return (
               <div className={styles['category-description']}>
-                <p>무박 2일간의 해커톤을 통해</p>
+                <p>무박 2일간의 해커톤을 통해 아이디어를 </p>
                 <p>
-                  <span>아이디어를 빠르게 프로덕트로 만들어보는 프로젝트,</span>
-                  <span>SOPTKATHON</span>
+                  <span>빠르게 프로덕트로 만들어보는 프로젝트,</span>
+                  <span className={styles['category-summary']}>SOPTKATHON</span>
                 </p>
               </div>
             );
@@ -133,7 +133,7 @@ function ProjectCategoryDescription(category: ProjectCategoryType) {
                 <p>4개월간의 장기 협업을 통해 </p>
                 <p>
                   <span>한 개의 온전한 프로덕트를 만드는 프로젝트,</span>
-                  <span>SOPT-TERM</span>
+                  <span className={styles['category-summary']}>SOPT-TERM</span>
                 </p>
               </div>
             );
@@ -143,7 +143,7 @@ function ProjectCategoryDescription(category: ProjectCategoryType) {
                 <p>2개월간 지식 공유를 진행하며</p>
                 <p>
                   <span>아이디어를 시각화 하거나 프로덕트로 만들어보는 프로젝트</span>
-                  <span>스터디</span>
+                  <span className={styles['category-summary']}>스터디</span>
                 </p>
               </div>
             );
@@ -153,7 +153,7 @@ function ProjectCategoryDescription(category: ProjectCategoryType) {
                 <p>기획, 디자인, 개발 파트가 배운 내용을 통해 </p>
                 <p>
                   <span>간단한 아이디어를 시각화 해보는 프로젝트, </span>
-                  <span>합동 세미나</span>
+                  <span className={styles['category-summary']}>합동 세미나</span>
                 </p>
               </div>
             );
