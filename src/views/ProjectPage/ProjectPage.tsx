668d8f82c93bcc0e174d10d64717415306f89c3b
leeseooo <leeseoooo@naver.com>
2022-11-18 01:11:17 +0900

    feat/#33: 폰트 및 글씨간격 수정

--- src/views/ProjectPage/ProjectPage.tsx
+++ src/views/ProjectPage/ProjectPage.tsx
@@ -3,6 +3,7 @@ import { Header, Footer, Layout } from '@src/components';
 import { ProjectFilter, ProjectList } from './components';
 import { ProjectCategoryType } from './lib/constants';
 import useFetch from './hooks/useFetch';
+import React from 'react';
 
 function Projects() {
   const [selectedCategory, setCategory] = useState<ProjectCategoryType>(ProjectCategoryType.ALL);
