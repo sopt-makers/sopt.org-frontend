5020ad7e2a079292810dd2f01e03708ac256932b
ingong <insong.lee@webtoonscorp.com>
2022-11-20 21:26:33 +0900

    feat: 프로젝트가 0개 일때 보여주는 UI 수정

--- src/views/ProjectPage/components/common/EmptyContent.tsx
+++ src/views/ProjectPage/components/common/EmptyContent.tsx
@@ -9,28 +9,28 @@ export function EmptyContent() {
   return (
     <section className={cc([styles.blank])}>
       <div className={cc([styles.contour])}>
-        <Circle />
-        <Triangle />
-        <Diagonal />
-        <Circle />
-        <Triangle />
-        <Diagonal />
-        <Circle />
-        <Triangle />
-        <Diagonal />
+        <IconGroup />
+        <IconGroup />
+        <IconGroup />
+        <IconGroup />
+        <IconGroup />
       </div>
       <p>아직 올라온 프로젝트가 없어요.</p>
       <div className={cc([styles.contour])}>
-        <Circle />
-        <Triangle />
-        <Diagonal />
-        <Circle />
-        <Triangle />
-        <Diagonal />
-        <Circle />
-        <Triangle />
-        <Diagonal />
+        <IconGroup />
+        <IconGroup />
+        <IconGroup />
+        <IconGroup />
+        <IconGroup />
       </div>
     </section>
   );
 }
+
+const IconGroup = () => (
+  <>
+    <Circle />
+    <Triangle />
+    <Diagonal />
+  </>
+);
