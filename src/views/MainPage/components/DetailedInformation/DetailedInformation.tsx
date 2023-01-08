bcfc6bab9b2c9235b51daadd1607239a9b889c13
ingong <insong.lee@webtoonscorp.com>
2023-01-08 22:48:06 +0900

    feat: DetailedInformation 모바일용 컴포넌트
    - 버튼 클릭 시 기획서에 요구된 동작에 맞게 이동 또는 새로운 페이지 열어줌
    - 마크업 및 스타일 적용
    - arrowRight svg 추가

--- src/views/MainPage/components/DetailedInformation/DetailedInformation.tsx
+++ src/views/MainPage/components/DetailedInformation/DetailedInformation.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+import { ReactComponent as ArrowRight } from '@src/views/MainPage/assets/arrow-right.svg';
 import styles from './detailed-information.module.scss';
 
 const cardList = [
@@ -16,15 +18,40 @@ const cardList = [
 ];
 
 export function DetailedInformation() {
+  const router = useRouter();
+  const handleClickItem = (name: string) => {
+    switch (name) {
+      case '프로젝트':
+        router.push('/project');
+        break;
+      case 'FAQ':
+        router.push('/FAQ');
+        break;
+      case 'Youtube':
+        window.open('https://www.youtube.com/c/SOPTMEDIA');
+        break;
+      default:
+        throw new Error('unKnown Name');
+    }
+  };
+
   return (
-    <section>
-      <h3>SOPT를 더 알고 싶다면, 둘러보세요.</h3>
-      <div>
+    <section className={styles.container}>
+      <h3 className={styles.title}>SOPT를 더 알고 싶다면, 둘러보세요.</h3>
+      <div className={styles.content}>
         {cardList.map(({ name, desc }) => {
           return (
-            <article key={name}>
-              <h4>{name}</h4>
-              <p>{desc}</p>
+            <article
+              className={styles.card}
+              onClick={() => handleClickItem(name)}
+              role="presentation"
+              key={name}
+            >
+              <div className={styles.nameWrapper}>
+                <h4 className={styles.name}>{name}</h4>
+                <ArrowRight />
+              </div>
+              <p className={styles.desc}>{desc}</p>
             </article>
           );
         })}
