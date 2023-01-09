57891f6f86a9238f1beb34693c6ca8f04ed0a9a2
ingong <insong.lee@webtoonscorp.com>
2023-01-09 22:02:31 +0900

    fix: corporate linked activities 제목 왼쪽맞춤 적용

--- src/views/MainPage/components/CorporateLinkedActivities/CorporateLinkedActivities.tsx
+++ src/views/MainPage/components/CorporateLinkedActivities/CorporateLinkedActivities.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import { ReactComponent as ArrowLeft } from '@src/views/MainPage/assets/arrow-left-28x28.svg';
 import { ReactComponent as ArrowRight } from '@src/views/MainPage/assets/arrow-right-28x28.svg';
 import { useHorizontalScroll } from '@src/views/MainPage/lib';
-import cc from 'classcat';
 import { corporatedLinkedActivities } from './constants';
 import styles from './corporate-linked-activities.module.scss';
 
