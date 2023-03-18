86571fb5059350ef0518a9f47837bc0e1d16a9e8
Seojin Kim <seojin.kim@voyagerx.com>
2023-03-19 00:23:13 +0900

    chore: add thumbnails

---
+++ src/assets/mainLogo/index.ts
@@ -0,0 +1,43 @@
+import logo11 from './logo_11.svg';
+import logo12 from './logo_12.svg';
+import logo14 from './logo_14.svg';
+import logo15 from './logo_15.svg';
+import logo16 from './logo_16.svg';
+import logo17 from './logo_17.svg';
+import logo18 from './logo_18.svg';
+import logo19 from './logo_19.svg';
+import logo20 from './logo_20.svg';
+import logo21 from './logo_21.svg';
+import logo22 from './logo_22.svg';
+import logo23 from './logo_23.svg';
+import logo24 from './logo_24.svg';
+import logo25 from './logo_25.svg';
+import logo26 from './logo_26.svg';
+import logo27 from './logo_27.svg';
+import logo28 from './logo_28.svg';
+import logo29 from './logo_29.svg';
+import logo30 from './logo_30.svg';
+import logo31 from './logo_31.svg';
+
+export {
+  logo11,
+  logo12,
+  logo14,
+  logo15,
+  logo16,
+  logo17,
+  logo18,
+  logo19,
+  logo20,
+  logo21,
+  logo22,
+  logo23,
+  logo24,
+  logo25,
+  logo26,
+  logo27,
+  logo28,
+  logo29,
+  logo30,
+  logo31,
+};
