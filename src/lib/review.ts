e3d06bee2c4d32ae0a7324b1999d33b6d048a2f3
SNUPI <joohaem@daum.net>
2023-03-28 16:19:53 +0900

    chore: separate useIntersectionObserver

--- src/lib/review.ts
+++ src/lib/review.ts
@@ -7,9 +7,9 @@ const client = axios.create({
   timeout: 3000,
 });
 
-export const getReviews = async (tab: TAB): Promise<ReviewType[]> => {
+export const getReviews = async (tab: TAB, pageNo = 1): Promise<ReviewType[]> => {
   const partParameter = tab === TAB.ALL ? {} : { part: tab };
-  const pageParameter = { pageNo: 1, limit: '120' };
+  const pageParameter = { pageNo, limit: 6 };
   const parameter = qs.stringify({ ...partParameter, ...pageParameter });
 
   const { data } = await client.get(`/reviews?${parameter}`);
