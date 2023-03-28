b80f79764db577749fea9b7e2671c7f847ddfdcf
SNUPI <joohaem@daum.net>
2023-03-28 17:07:29 +0900

    feat: 무한스크롤 및 stacked 패칭 커스텀 훅 추가

---
+++ src/hooks/useStackedFetchBase.ts
@@ -0,0 +1,41 @@
+import { to } from 'await-to-js';
+import { useEffect, useReducer, useRef } from 'react';
+import { reducer } from './useFetchBase/reducer';
+import { Action, State } from './useFetchBase/types';
+
+function useStackedFetchBase<T>(willFetch: () => Promise<T[]>): State<T> {
+  const [state, dispatch] = useReducer<React.Reducer<State<T>, Action<T>>>(reducer, {
+    _TAG: 'IDLE',
+  });
+  const stateStack = useRef<T[]>([]);
+
+  useEffect(() => {
+    const fetchProjectList = async () => {
+      dispatch({
+        _TAG: 'FETCH',
+      });
+
+      const [error, response] = await to(willFetch());
+
+      if (error) {
+        return dispatch({
+          _TAG: 'FAILED',
+          error,
+        });
+      }
+
+      if (response) {
+        const newState = [...stateStack.current, ...response];
+
+        dispatch({ _TAG: 'SUCCESS', data: newState });
+        stateStack.current = newState;
+      }
+    };
+
+    fetchProjectList();
+  }, [willFetch]);
+
+  return state;
+}
+
+export default useStackedFetchBase;
---
+++ src/views/ReviewPage/hooks/useInfiniteScroll.ts
@@ -0,0 +1,17 @@
+import useIntersectionObserver from '@src/hooks/useIntersectionObserver';
+import { useState } from 'react';
+
+// TODO :: 프로필 훅과 연동
+export default function useInfiniteScroll() {
+  const [count, setCount] = useState(1);
+
+  const ref = useIntersectionObserver(
+    async (entry, observer) => {
+      setCount(prevCount => prevCount + 1);
+      observer.unobserve(entry.target);
+    },
+    { threshold: 0.25, rootMargin: '80px' },
+  );
+
+  return { ref, count };
+}
