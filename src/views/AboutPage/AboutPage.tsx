38fbefd29e9bf6bd21caae370e7097fc575859bb
SNUPI <joohaem@daum.net>
2023-04-28 01:28:48 +0900

    feat: add about page

---
+++ src/views/AboutPage/AboutPage.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import { Header, Layout } from '@src/components';
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <Header />
+      <Root></Root>
+    </Layout>
+  );
+};
+
+export default AboutPage;
+
+const Root = styled.main``;
