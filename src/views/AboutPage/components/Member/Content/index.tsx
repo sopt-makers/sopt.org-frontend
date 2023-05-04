e176e33d9b0c1aca47ec85f032c95ade46d5fa10
SNUPI <joohaem@daum.net>
2023-05-04 14:27:48 +0900

    feat: mock fetching member datas

--- src/views/AboutPage/components/Member/Content/index.tsx
+++ src/views/AboutPage/components/Member/Content/index.tsx
@@ -2,24 +2,28 @@ import { useState } from 'react';
 import Flex from '@src/components/common/Flex';
 import { ExtraPart } from '@src/lib/types/universal';
 import { PartExtraType } from '@src/lib/types/universal';
+import useFetchMember from '@src/views/AboutPage/hooks/useFetchMemeber';
 import TabBar from '../../common/TabBar';
 import MemberCard from '../Card';
 import * as St from './style';
 
 const MemberContent = () => {
   const [currentPart, setCurrentPart] = useState<ExtraPart>(PartExtraType.ALL);
+  const state = useFetchMember(currentPart);
+
+  if (state._TAG !== 'OK') return null;
 
   return (
     <Flex dir="column" gap={{ mobile: 18, tablet: 24, desktop: 48 }}>
       <TabBar onTabClick={setCurrentPart} selectedTab={currentPart} includesExtra={true} />
       <St.CardContainer>
-        {Array.from({ length: 9 }).map((member, i) => (
+        {state.data.members.map((member, idx) => (
           <MemberCard
-            key={i}
-            imgSrc="https://avatars.githubusercontent.com/u/47105088?v=4"
-            name="박이정"
-            description="안채량박서원최영훈"
-            part="Android 파트장"
+            key={`${member.name}-${idx}`}
+            imgSrc={member.src}
+            name={member.name}
+            description={member.description}
+            part={member.part}
           />
         ))}
       </St.CardContainer>
