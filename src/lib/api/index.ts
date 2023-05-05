d899f6bfcd1d234dfd4525a3787176cf1f78e89b
SNUPI <joohaem@daum.net>
2023-05-05 13:49:30 +0900

    feat: link remote about api

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,10 +1,10 @@
 import { API } from '../types/universal';
-import { mockAboutAPI } from './mock/about';
+import { remoteAboutAPI } from './remote/about';
 import { remoteProjectAPI } from './remote/project';
 import { remoteReviewAPI } from './remote/review';
 
 export const api: API = {
   projectAPI: remoteProjectAPI,
   reviewAPI: remoteReviewAPI,
-  aboutAPI: mockAboutAPI,
+  aboutAPI: remoteAboutAPI,
 };
