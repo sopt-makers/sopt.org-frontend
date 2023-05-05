df2de9eeb5833daa127786775d82848406edf13e
SNUPI <joohaem@daum.net>
2023-05-05 21:12:55 +0900

    chore: set Environment Variable

--- src/utils/const/client.ts
+++ src/utils/const/client.ts
@@ -1,6 +1,2 @@
-const BASE_DEV_URL = 'https://api-dev.sopt.org';
-const BASE_PROD_URL = 'https://api.sopt.org';
-
-export const BASE_URL = process.env.NODE_ENV === 'development' ? BASE_DEV_URL : BASE_PROD_URL;
-
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const DEFAULT_TIMEOUT = 3000;
