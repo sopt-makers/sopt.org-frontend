6a23805737866330b5b6eeef5dceed79502b559a
SNUPI <joohaem@daum.net>
2023-05-08 14:08:53 +0900

    chore: 활동 멤버 이름/파트 필수 요소

--- src/views/AboutPage/components/Member/Card/index.tsx
+++ src/views/AboutPage/components/Member/Card/index.tsx
@@ -3,18 +3,18 @@ import * as St from './style';
 
 type MeberCardProps = {
   imgSrc?: string;
-  name?: string;
+  name: string;
   description?: string;
-  part?: string;
+  part: string;
 };
 
 const MemberCard = ({ imgSrc, name, description, part }: MeberCardProps) => {
   return (
     <St.Card>
-      <St.ImageWrapper>{imgSrc && <Image src={imgSrc} alt={name ?? '-'} fill />}</St.ImageWrapper>
-      <St.Name>{name ?? '-'}</St.Name>
+      <St.ImageWrapper>{imgSrc && <Image src={imgSrc} alt={name} fill />}</St.ImageWrapper>
+      <St.Name>{name}</St.Name>
       <St.Desc>{description ?? '-'}</St.Desc>
-      <St.Part>{part ?? '-'}</St.Part>
+      <St.Part>{part}</St.Part>
     </St.Card>
   );
 };
