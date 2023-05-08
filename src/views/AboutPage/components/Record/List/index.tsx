698d4a7bcbf0fc5f9165c3810acb44242e737425
SNUPI <joohaem@daum.net>
2023-05-08 16:50:41 +0900

    chore: 프로젝트 진행중 표시

--- src/views/AboutPage/components/Record/List/index.tsx
+++ src/views/AboutPage/components/Record/List/index.tsx
@@ -8,7 +8,11 @@ const RecordList = (props: RecordListProps) => {
   return (
     <Flex dir="row" gap={{ mobile: 8, tablet: 20, desktop: 30 }}>
       <RecordItem title="활동 멤버" count={`${props.records.memberCount}명`} href="/" />
-      <RecordItem title="프로젝트" count={`${props.records.projectCount}개`} href="/project" />
+      <RecordItem
+        title="프로젝트"
+        count={props.records.projectCount === 0 ? '진행중' : `${props.records.projectCount}개`}
+        href="/project"
+      />
       <RecordItem title="스터디" count={`${props.records.studyCount}개`} href="/study" />
     </Flex>
   );
