9031bc080ba95b62ab8fb265cf77f450ff0754f8
SNUPI <joohaem@daum.net>
2023-05-08 20:32:57 +0900

    style: member card empty value

--- src/views/AboutPage/components/Member/Card/index.tsx
+++ src/views/AboutPage/components/Member/Card/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import NullImage from '@src/assets/images/null_image.png';
 import * as St from './style';
 
 type MeberCardProps = {
@@ -11,9 +12,11 @@ type MeberCardProps = {
 const MemberCard = ({ imgSrc, name, description, part }: MeberCardProps) => {
   return (
     <St.Card>
-      <St.ImageWrapper>{imgSrc && <Image src={imgSrc} alt={name} fill />}</St.ImageWrapper>
+      <St.ImageWrapper>
+        <Image src={imgSrc || NullImage.src} alt={name} fill />
+      </St.ImageWrapper>
       <St.Name>{name}</St.Name>
-      <St.Desc>{description ?? ' '}</St.Desc>
+      <St.Desc>{description || '-'}</St.Desc>
       <St.Part>{part}</St.Part>
     </St.Card>
   );
