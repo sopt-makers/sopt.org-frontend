12fa6186a7caf2f80f4ba9dd09950754ebb7c3bf
Seojin Kim <1106laura@naver.com>
2023-05-11 01:36:12 +0900

    feat: scroll to member section when member record is clicked

--- src/views/AboutPage/components/Record/List/index.tsx
+++ src/views/AboutPage/components/Record/List/index.tsx
@@ -7,7 +7,7 @@ type RecordListProps = Pick<AboutInfoType, 'records'>;
 const RecordList = (props: RecordListProps) => {
   return (
     <Flex dir="row" gap={{ mobile: 8, tablet: 20, desktop: 30 }}>
-      <RecordItem title="활동 멤버" count={`${props.records.memberCount}명`} href="/" />
+      <RecordItem title="활동 멤버" count={`${props.records.memberCount}명`} href="#members" />
       <RecordItem
         title="프로젝트"
         count={props.records.projectCount === 0 ? '진행중' : `${props.records.projectCount}개`}
