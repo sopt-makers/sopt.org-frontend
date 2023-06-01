616a8625c11557db51032c7386dd33c455e9d553
SNUPI <joohaem@daum.net>
2023-06-01 19:00:19 +0900

    feat: About Members Error UI

---
+++ src/components/DataErrorBanner/index.tsx
@@ -0,0 +1,13 @@
+const DataErrorBanner = () => {
+  return (
+    <article style={{ width: '200px', height: '30px', backgroundColor: 'green' }}>
+      <img />
+      <div>
+        <strong></strong>
+        <p></p>
+      </div>
+    </article>
+  );
+};
+
+export default DataErrorBanner;
--- src/views/AboutPage/components/Member/Content/index.tsx
+++ src/views/AboutPage/components/Member/Content/index.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import DataErrorBanner from '@src/components/DataErrorBanner';
 import Flex from '@src/components/common/Flex';
+import { useIsMobile, useIsTablet } from '@src/hooks/useDevice';
 import { ExtraPart } from '@src/lib/types/universal';
 import { PartExtraType } from '@src/lib/types/universal';
+import { emptyMembers } from '@src/views/AboutPage/constant/emptyMembers';
 import useFetchMember from '@src/views/AboutPage/hooks/useFetchMemeber';
 import { OvalSpinner } from '@src/views/ProjectPage/components';
 import TabBar from '../../common/TabBar';
@@ -12,20 +15,40 @@ const MemberContent = () => {
   const [currentPart, setCurrentPart] = useState<ExtraPart>(PartExtraType.ALL);
   const state = useFetchMember(currentPart);
 
+  const isTablet = useIsTablet();
+  const isMobile = useIsMobile();
+
+  const memberCardsCount = useMemo(() => {
+    if (isMobile) return 2;
+    if (isTablet) return 3;
+    return 4;
+  }, [isMobile, isTablet]);
+
+  const cardContent = useMemo(() => {
+    if (state._TAG === 'OK')
+      return state.data.members.map(({ id, name, src, description, part }) => (
+        <MemberCard key={id} imgSrc={src} name={name} description={description} part={part} />
+      ));
+
+    if (state._TAG === 'ERROR')
+      return emptyMembers(memberCardsCount).map(({ id, name, src, description, part }) => (
+        <MemberCard key={id} imgSrc={src} name={name} description={description} part={part} />
+      ));
+
+    return (
+      <St.OvalSpinnerWrapper>
+        <OvalSpinner />
+      </St.OvalSpinnerWrapper>
+    );
+  }, [state, memberCardsCount]);
+
   return (
     <Flex dir="column" gap={{ mobile: 18, tablet: 24, desktop: 48 }}>
-      <TabBar onTabClick={setCurrentPart} selectedTab={currentPart} includesExtra={true} />
-      <St.CardContainer>
-        {state._TAG === 'OK' ? (
-          state.data.members.map(({ id, name, src, description, part }) => (
-            <MemberCard key={id} imgSrc={src} name={name} description={description} part={part} />
-          ))
-        ) : (
-          <St.OvalSpinnerWrapper>
-            <OvalSpinner />
-          </St.OvalSpinnerWrapper>
-        )}
-      </St.CardContainer>
+      {state._TAG === 'ERROR' && <DataErrorBanner />}
+      {!(state._TAG === 'ERROR' && isMobile) && (
+        <TabBar onTabClick={setCurrentPart} selectedTab={currentPart} includesExtra={true} />
+      )}
+      <St.CardContainer>{cardContent}</St.CardContainer>
     </Flex>
   );
 };
