562434bb74171c221b80545fcdddb255025dce96
solar3070 <>
2023-09-05 03:31:18 +0900

    fix: 솝트 심볼 이미지 변경

--- src/views/MainPage/components/RecruitFloatingBanner/RecruitFloatingBanner.tsx
+++ src/views/MainPage/components/RecruitFloatingBanner/RecruitFloatingBanner.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Timer from '@src/components/common/Timer';
 import { useIsMobile } from '@src/hooks/useDevice';
-import SoptSymbol from '@src/views/MainPage/assets/sopt-symbol.svg';
+import SoptSymbol from '@src/views/MainPage/assets/dosopt_symbol.png';
 import dayjs from 'dayjs';
 
 const TARGET_DATE = dayjs('2023-09-09T18:00:00+09:00').toDate();
@@ -16,7 +16,7 @@ export function RecruitFloatingBanner() {
     <Wrapper>
       <Banner isMobile={isMobile} onClick={() => router.push('/recruit')}>
         <Countdown>
-          <Symbol src={SoptSymbol} alt="솝트 심볼" />
+          <Symbol src={SoptSymbol.src} alt="솝트 심볼" />
           {isMobile ? (
             '33기 YB 지원하기'
           ) : (
