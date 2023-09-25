2b2ae2be99901c0858b2bf9a85e5b0a481c1c05a
SeojinSeojin <1106laura@naver.com>
2023-09-25 23:24:51 +0900

    feat: add amplitude trackings in review page

--- src/views/ReviewPage/components/Content/index.tsx
+++ src/views/ReviewPage/components/Content/index.tsx
@@ -1,3 +1,4 @@
+import { track } from '@amplitude/analytics-browser';
 import { useState } from 'react';
 import { ExtraPart, PartExtraType } from '@src/lib/types/universal';
 import Reviews from '../Reviews';
@@ -6,9 +7,14 @@ import TabBar from '../TabBar';
 const Content = () => {
   const [selectedTab, setSelectedTab] = useState<ExtraPart>(PartExtraType.ALL);
 
+  const onTabClick = (tab: ExtraPart) => {
+    track('click_review_part', { part: tab });
+    setSelectedTab(tab);
+  };
+
   return (
     <>
-      <TabBar onTabClick={setSelectedTab} selectedTab={selectedTab} />
+      <TabBar onTabClick={onTabClick} selectedTab={selectedTab} />
       <Reviews selectedTab={selectedTab} />
     </>
   );
