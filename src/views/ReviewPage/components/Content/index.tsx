93daa18dd90be22cb5da95c6d6d66d1e3b524c54
SeojinSeojin <1106laura@naver.com>
2023-09-28 22:31:31 +0900

    fix: remove duplicate tracks

--- src/views/ReviewPage/components/Content/index.tsx
+++ src/views/ReviewPage/components/Content/index.tsx
@@ -1,20 +1,14 @@
-import { track } from '@amplitude/analytics-browser';
-import { useState } from 'react';
 import { ExtraPart, PartExtraType } from '@src/lib/types/universal';
+import { useState } from 'react';
 import Reviews from '../Reviews';
 import TabBar from '../TabBar';
 
 const Content = () => {
   const [selectedTab, setSelectedTab] = useState<ExtraPart>(PartExtraType.ALL);
 
-  const onTabClick = (tab: ExtraPart) => {
-    track('click_review_part', { part: tab });
-    setSelectedTab(tab);
-  };
-
   return (
     <>
-      <TabBar onTabClick={onTabClick} selectedTab={selectedTab} />
+      <TabBar onTabClick={setSelectedTab} selectedTab={selectedTab} />
       <Reviews selectedTab={selectedTab} />
     </>
   );
