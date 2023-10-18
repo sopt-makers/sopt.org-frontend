94a89eeec8052355480837775976e9797e3bf053
f0ever0 <thddndud0830@gmail.com>
2023-10-18 15:19:28 +0900

    feat : 블로그 탭 네비게이션

--- src/views/BlogPage/BlogPage.tsx
+++ src/views/BlogPage/BlogPage.tsx
@@ -1,7 +1,12 @@
 import PageLayout from '@src/components/common/PageLayout';
+import Navigation from './components/navigation';
 
 function BlogPage() {
-  return <PageLayout></PageLayout>;
+  return (
+    <PageLayout>
+      <Navigation />
+    </PageLayout>
+  );
 }
 
 export default BlogPage;
