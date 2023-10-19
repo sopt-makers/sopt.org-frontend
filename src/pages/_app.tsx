9483ab4548fcd040ce5d4a90416127a5049bbe1d
f0ever0 <thddndud0830@gmail.com>
2023-10-19 20:41:23 +0900

    feat : 프로젝트 디테일 페이지 getServerSideProps 사용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import * as amplitude from '@amplitude/analytics-browser';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { Global, ThemeProvider } from '@emotion/react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import HTMLHead from '@src/components/common/HTMLHead';
+import SEO from '@src/components/common/SEO';
 import GoogleTagManagerNoscript from '@src/components/googleTagManager/Noscript';
 import GoogleTagManagerScript from '@src/components/googleTagManager/Script';
 import * as gtm from '@src/components/googleTagManager/gtm';
@@ -33,7 +34,45 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <HTMLHead />
+      <Head>
+        <title>SOPT</title>
+        <meta
+          name="description"
+          content="SOPT는 IT와 벤처 창업에 뜻이 있는 대학생들이 모인 국내 최대 규모의 대학생 연합 IT 벤처 창업 동아리입니다."
+        />
+        <meta name="title" content="SOPT" />
+        <meta name="keyword" content="IT, 창업, 개발, 대학생, 동아리" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <meta name="apple-mobile-web-app-title" content="SOPT" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0,  maximum-scale=1"
+        />
+        <meta content="yes" name="apple-mobile-web-app-capable" />
+      </Head>
+      <SEO />
       <GoogleTagManagerScript />
       <Global styles={global} />
       <ThemeProvider theme={theme}>
---
+++ src/pages/project/[id]/index.tsx
@@ -0,0 +1,165 @@
+import { NextPageContext } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { ReactComponent as IToggle } from '@src/assets/images/toggle.svg';
+import PageLayout from '@src/components/common/PageLayout';
+import SEO from '@src/components/common/SEO';
+import { api } from '@src/lib/api';
+import { GetProjectDetailResponse, ProjectLinkType } from '@src/lib/types/project';
+import { dateFormat } from '@src/utils/dateFormat';
+import * as S from '@src/views/ProjectDetailPage/ProjectDetail.style';
+import { TeamMembersType } from '@src/views/ProjectDetailPage/types';
+import { getLinkNameAndSrcWithType } from '@src/views/ProjectDetailPage/utils/getLinkNameAndSrcWithType';
+
+const projectOverviewTitle = [
+  '프로젝트 기간',
+  '진행 기수',
+  '프로젝트 형태',
+  '프로젝트 상태',
+  '프로젝트 링크',
+];
+
+function ProjectDetailPage({ project }: GetProjectDetailResponse) {
+  const [isOverviewOpened, setIsOverviewOpened] = useState(true);
+  const [isTeamMemberOpened, setIsTeamMemberOpened] = useState(false);
+  if (!project) return;
+
+  const {
+    projectImage,
+    logoImage,
+    name,
+    summary,
+    generation,
+    serviceTypes,
+    startAt,
+    endAt,
+    isFounding,
+    isAvailable,
+    link,
+    members,
+    detail,
+  } = project;
+
+  return (
+    <>
+      <SEO projectTitle={name} projectImageURL={projectImage} />
+
+      <PageLayout showScrollTopButton>
+        <S.Root>
+          <S.Container>
+            <S.ProjectHeader>
+              <S.LogoImageWrapper>
+                <Image src={logoImage} alt={`${name} 프로젝트 로고`} width="72" height="72" />
+              </S.LogoImageWrapper>
+              <div>
+                <h1>{name}</h1>
+                <p>{summary}</p>
+              </div>
+            </S.ProjectHeader>
+            {projectImage && (
+              <S.ProjectImageWrapper>
+                <Image src={projectImage} alt={`${name} 프로젝트 썸네일`} fill />
+              </S.ProjectImageWrapper>
+            )}
+            <S.ProjectWrapper>
+              <S.ToggleSection>
+                <S.ProjectOverview isOverviewOpened={isOverviewOpened}>
+                  <S.ToggleWrapper onClick={() => setIsOverviewOpened((prev) => !prev)}>
+                    <S.Title>프로젝트 요약</S.Title>
+                    <button type="button">
+                      <S.OverviewToggleImage isOverviewOpened={isOverviewOpened}>
+                        <IToggle />
+                      </S.OverviewToggleImage>
+                    </button>
+                  </S.ToggleWrapper>
+                  <S.ProjectOverviewDetail isOverviewOpened={isOverviewOpened}>
+                    <S.ProjectInfo isLinkExist={link.length !== 0}>
+                      <div>
+                        {projectOverviewTitle.map((title) => (
+                          <h1 key={title}>{title}</h1>
+                        ))}
+                      </div>
+
+                      <div>
+                        <span>
+                          {dateFormat(startAt)} - {dateFormat(endAt) || '진행중'}
+                        </span>
+                        <span>{generation ? generation + '기' : '-'}</span>
+                        <span>
+                          {serviceTypes
+                            .map((type: string) => (type === 'WEB' ? '웹' : '앱'))
+                            .join(', ')}
+                        </span>
+                        <span>
+                          {!isAvailable && !isFounding
+                            ? '-'
+                            : isAvailable && !isFounding
+                            ? '운영중'
+                            : !isAvailable && isFounding
+                            ? '창업중'
+                            : '운영중, 창업중'}
+                        </span>
+                        <span>{link.length ? link.length + '개' : '-'}</span>
+                      </div>
+                    </S.ProjectInfo>
+                    <S.ProjectLinkWrapper>
+                      {link.map(({ title, url }: ProjectLinkType) => {
+                        const { name, src } = getLinkNameAndSrcWithType(title);
+                        return (
+                          <Link href={url} key={title}>
+                            <S.ProjectLink>
+                              <S.ImageWrapper>
+                                <Image src={src} alt={`${name} 로고`} width="56" height="56" />
+                              </S.ImageWrapper>
+                              <span>{name}</span>
+                            </S.ProjectLink>
+                          </Link>
+                        );
+                      })}
+                    </S.ProjectLinkWrapper>
+                  </S.ProjectOverviewDetail>
+                </S.ProjectOverview>
+                <S.ProjectTeam>
+                  <S.ToggleWrapper onClick={() => setIsTeamMemberOpened((prev) => !prev)}>
+                    <S.Title>프로젝트 팀원</S.Title>
+                    <button type="button">
+                      <S.TeamMemberToggleImage isTeamMemberOpened={isTeamMemberOpened}>
+                        <IToggle />
+                      </S.TeamMemberToggleImage>
+                    </button>
+                  </S.ToggleWrapper>
+                  <S.TeamMembers isTeamMemberOpened={isTeamMemberOpened}>
+                    {members.map(({ name, role, description }: TeamMembersType, idx: number) => (
+                      <S.Members key={idx}>
+                        <h1>{role}</h1>
+                        <S.MemberDetail>
+                          <h1>{name}</h1>
+                          <p>{description}</p>
+                        </S.MemberDetail>
+                      </S.Members>
+                    ))}
+                  </S.TeamMembers>
+                </S.ProjectTeam>
+              </S.ToggleSection>
+              <S.ProjectDescription>
+                <S.Title>프로젝트 설명</S.Title>
+                <p>{detail}</p>
+              </S.ProjectDescription>
+            </S.ProjectWrapper>
+          </S.Container>
+        </S.Root>
+      </PageLayout>
+    </>
+  );
+}
+
+export default ProjectDetailPage;
+
+export const getServerSideProps = async ({ query }: NextPageContext) => {
+  const id = query.id as string;
+  const { project } = await api.projectAPI.getProjectDetail(+id);
+  return {
+    props: { project },
+  };
+};
