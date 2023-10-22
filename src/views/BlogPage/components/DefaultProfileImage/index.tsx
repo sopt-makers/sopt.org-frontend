8edccd26f0378c7be224990ba93f76bf14a861ad
solar3070 <>
2023-10-22 17:07:29 +0900

    feat: 기본 프로필 이미지 컴포넌트 구현

---
+++ src/views/BlogPage/components/DefaultProfileImage/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import icProfileDefault from '@src/assets/icons/ic_profile_default.svg';
+import * as S from './style';
+
+function DefaultProfileImage() {
+  return (
+    <S.DefaultProfileImage>
+      <Image src={icProfileDefault} alt="작성자 프로필 이미지" width={10} height={10} />
+    </S.DefaultProfileImage>
+  );
+}
+
+export default DefaultProfileImage;
