ea72fedd5421ac99c2263901dbe9fe3402d7a302
f0ever0 <thddndud0830@gmail.com>
2023-10-24 15:14:23 +0900

    chore : 블로그 type 변경

--- src/views/BlogPage/components/BlogTab/index.tsx
+++ src/views/BlogPage/components/BlogTab/index.tsx
@@ -8,21 +8,21 @@ import {
 } from '@src/lib/constants/tabs';
 import { ExtraPart, PartExtraType } from '@src/lib/types/universal';
 import * as S from './style';
-import { BlogTabList } from './types';
+import { BlogTabMap, BlogTabType } from './types';
 
 export default function BlogTab() {
-  const [selectedTab, setSelectedTab] = useState<keyof BlogTabList>('REVIEW');
+  const [selectedTab, setSelectedTab] = useState<BlogTabType>(BlogTabType.REVIEW);
   const [selectedMajorCategory, setMajorCategory] = useState<number>(
     activeGenerationCategoryList[0],
   );
   const [selectedSubCategory, setSubCategory] = useState<ExtraPart>(PartExtraType.ALL);
 
-  const blogTabList: BlogTabList = {
-    REVIEW: {
+  const blogTabList: BlogTabMap = {
+    review: {
       title: '활동후기',
       description: '회원들의 진솔한 후기를 통해 SOPT를 미리 만나보세요.  ',
     },
-    ARTICLE: {
+    article: {
       title: '아티클',
       description: '회원들의 아티클을 통해 SOPT에서 얻은 인사이트를 확인해보세요.',
     },
@@ -33,10 +33,12 @@ export default function BlogTab() {
       <S.Container>
         <S.TabContainer>
           {Object.entries(blogTabList).map(([blogTab, tabInfo]) => {
+            console.log(blogTab);
+            console.log(tabInfo);
             return (
               <S.TabTitle
                 key={blogTab}
-                onClick={() => setSelectedTab(blogTab as keyof BlogTabList)}
+                onClick={() => setSelectedTab(blogTab as BlogTabType)}
                 isSelected={selectedTab === blogTab}
               >
                 {tabInfo.title}
--- src/views/BlogPage/components/BlogTab/types.ts
+++ src/views/BlogPage/components/BlogTab/types.ts
@@ -1,10 +1,11 @@
-export interface BlogTabList {
-  REVIEW: {
-    title: string;
-    description: string;
-  };
-  ARTICLE: {
-    title: string;
-    description: string;
-  };
+export enum BlogTabType {
+  REVIEW = 'review',
+  ARTICLE = 'article',
 }
+
+type BlogTabContent = {
+  title: string;
+  description: string;
+};
+
+export type BlogTabMap = Record<BlogTabType, BlogTabContent>;
