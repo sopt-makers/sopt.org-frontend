c06e8dc842dd5d8c095f272d9d4c7065cbc59bc3
solar3070 <>
2023-10-29 04:01:58 +0900

    style: 모바일 반응형 브레이크 포인트 수정

--- src/views/ProjectPage/styles.ts
+++ src/views/ProjectPage/styles.ts
@@ -9,7 +9,7 @@ const SectionTitle = styled.div`
   letter-spacing: -0.56px;
   margin-bottom: 40px;
 
-  @media (max-width: 765.9px) {
+  @media (max-width: 899px) {
     font-size: 18px;
     font-weight: 700;
     letter-spacing: -0.36px;
@@ -29,27 +29,23 @@ const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
 
-  width: 1200px;
+  width: 1112px;
   padding: 100px 0;
 
   /* 태블릿 뷰 */
-  @media (max-width: 1299px) and (min-width: 1124px) {
-    width: 1080px;
-  }
-
-  @media (max-width: 1124px) and (min-width: 766px) {
-    width: 700px;
+  @media (max-width: 1279px) and (min-width: 900px) {
+    width: 732px;
   }
 
   /* 모바일 뷰 */
-  @media (max-width: 765.9px) {
+  @media (max-width: 899px) {
     width: 360px;
   }
 `;
 
 const Spacing = styled.div`
   height: 108px;
-  @media (max-width: 765.9px) {
+  @media (max-width: 899px) {
     height: 48px;
   }
 `;
@@ -57,9 +53,10 @@ const Spacing = styled.div`
 const FilterWrapper = styled.div`
   display: flex;
   gap: 10px;
+  margin-bottom: 16px;
 
   /* 모바일 뷰 */
-  @media (max-width: 767px) {
+  @media (max-width: 899px) {
     gap: 7px;
   }
 `;
