cc67428e7d3ede40e754bacfcd12211796d7fce6
f0ever0 <thddndud0830@gmail.com>
2023-10-28 10:58:28 +0900

    chore : build 에러 해결을 위한 constant/tab 추가

--- src/lib/constants/tabs.ts
+++ src/lib/constants/tabs.ts
@@ -1,4 +1,38 @@
 import { PartCategoryType } from '@src/lib/types/blog';
+import { ExtraTabType, Part, PartExtraType, TabType } from '../types/universal';
+
+export const extraTabs: ExtraTabType[] = [
+  {
+    value: PartExtraType.ALL,
+    label: '전체',
+  },
+  {
+    value: Part.PLAN,
+    label: '기획',
+  },
+  {
+    value: Part.DESIGN,
+    label: '디자인',
+  },
+  {
+    value: Part.ANDROID,
+    label: '안드로이드',
+  },
+  {
+    value: Part.IOS,
+    label: 'iOS',
+  },
+  {
+    value: Part.WEB,
+    label: '웹',
+  },
+  {
+    value: Part.SERVER,
+    label: '서버',
+  },
+];
+
+export const tabs = extraTabs.slice(1) as TabType[];
 
 export const activeGenerationCategoryList: number[] = [0];
 
