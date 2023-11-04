d018784ff40adc5236b9bfe932a5c88befb168be
solar3070 <>
2023-11-05 01:41:55 +0900

    fix: 최근 출시한 프로젝트 useQuery 적용

--- src/views/ProjectPage/components/RecentProjectList/index.tsx
+++ src/views/ProjectPage/components/RecentProjectList/index.tsx
@@ -1,31 +1,21 @@
-import { useQueryClient } from 'react-query';
 import { ProjectCategoryType, ProjectPlatformType, ProjectType } from '@src/lib/types/project';
-import { sortBy } from '@src/lib/utils/array';
+import { useGetProjectList } from '@src/views/ProjectPage/hooks/queries';
 import S from '../../styles';
 import RecentProjectListCarousel from './Carousel';
 import RecentProjectListItem from './Item';
 
 export default function RecentProjectList() {
-  const queryClient = useQueryClient();
-  const projectList = queryClient.getQueryData<ProjectType[]>([
-    'getProjectList',
-    ProjectCategoryType.ALL,
-    ProjectPlatformType.ALL,
-  ]);
-  const recentProjectList = projectList
-    ? sortBy<ProjectType>(projectList, 'updatedAt')
-    : projectList;
+  const { data } = useGetProjectList(ProjectCategoryType.ALL, ProjectPlatformType.ALL, 'updatedAt');
+  const recentProjectList = data as ProjectType[];
 
   return (
     <>
       <S.SectionTitle>최근 출시한 프로젝트</S.SectionTitle>
-      {recentProjectList && (
-        <RecentProjectListCarousel>
-          {recentProjectList.slice(0, 6).map((d) => (
-            <RecentProjectListItem key={d.id} {...d} />
-          ))}
-        </RecentProjectListCarousel>
-      )}
+      <RecentProjectListCarousel>
+        {recentProjectList.slice(0, 6).map((project) => (
+          <RecentProjectListItem key={project.id} {...project} />
+        ))}
+      </RecentProjectListCarousel>
     </>
   );
 }
--- src/views/ProjectPage/hooks/queries/index.ts
+++ src/views/ProjectPage/hooks/queries/index.ts
@@ -1,10 +1,16 @@
 import { useQuery } from 'react-query';
 import { api } from '@src/lib/api';
-import { ProjectCategoryType, ProjectPlatformType } from '@src/lib/types/project';
+import { ProjectCategoryType, ProjectPlatformType, ProjectType } from '@src/lib/types/project';
+import { sortBy } from '@src/lib/utils/array';
 
-export const useGetProjectList = (category: ProjectCategoryType, platform: ProjectPlatformType) => {
+export const useGetProjectList = (
+  category: ProjectCategoryType,
+  platform: ProjectPlatformType,
+  sortType?: string,
+) => {
   const queryKey = ['getProjectList', category, platform];
   return useQuery(queryKey, () => api.projectAPI.getProjectList(category, platform), {
+    select: (data) => (sortType ? sortBy<ProjectType>(data, 'updatedAt') : data),
     staleTime: 30000,
   });
 };
