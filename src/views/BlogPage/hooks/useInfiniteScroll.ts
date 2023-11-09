239364885f79a6a68926781a6da1adffb9b3deef
SeojinSeojin <1106laura@naver.com>
2023-11-09 14:42:27 +0900

    fix CI errors

--- src/views/BlogPage/hooks/useInfiniteScroll.ts
+++ src/views/BlogPage/hooks/useInfiniteScroll.ts
@@ -1,6 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { FetchNextPageOptions, InfiniteQueryObserverResult } from 'react-query';
-import useBooleanState from '@src/hooks/useBooleanState';
 import useIntersectionObserver from '@src/hooks/useIntersectionObserver';
 import { BlogResponse } from '@src/lib/types/blog';
 
@@ -8,23 +7,23 @@ export default function useInfiniteScroll(fetchNextPage: {
   (options?: FetchNextPageOptions): Promise<InfiniteQueryObserverResult<BlogResponse>>;
   (): void;
 }) {
-  const [hasObserved, setHasObserved, setHasUnObserved] = useBooleanState(false);
+  const [hasObserved, setHasObserved] = useState(false);
 
   const ref = useIntersectionObserver(
     async (entry, observer) => {
       if (!hasObserved && entry.isIntersecting) {
         fetchNextPage();
-        setHasObserved();
+        setHasObserved(true);
       }
 
       observer.unobserve(entry.target);
-      setHasUnObserved();
+      setHasObserved(false);
     },
     { rootMargin: '80px' },
   );
 
   useEffect(() => {
-    setHasUnObserved();
+    setHasObserved(false);
   }, []);
 
   return { ref };
