e8a9f0c1093d968f23af823c5475c6dae3125990
solar3070 <>
2023-11-13 09:23:51 +0900

    fix: date fetching 코드 위치 수정

--- src/views/ProjectPage/components/RecentProjectList/Carousel/index.tsx
+++ src/views/ProjectPage/components/RecentProjectList/Carousel/index.tsx
@@ -1,8 +1,14 @@
 import Carousel from '@src/components/common/Carousel';
 import { useDeviceType, useIsDesktop, useIsMobile } from '@src/hooks/useDevice';
+import { ProjectCategoryType, ProjectPlatformType, ProjectType } from '@src/lib/types/project';
 import { CarouselArrowType, CarouselOverflowType } from '@src/lib/types/universal';
+import RecentProjectListItem from '@src/views/ProjectPage/components/RecentProjectList/Item';
+import { useGetProjectList } from '@src/views/ProjectPage/hooks/queries';
 
-export default function RecentProjectListCarousel({ children }: { children: JSX.Element[] }) {
+export default function RecentProjectListCarousel() {
+  const { data } = useGetProjectList(ProjectCategoryType.ALL, ProjectPlatformType.ALL);
+  const recentProjectList = data as ProjectType[];
+
   const isDesktopSize = useIsDesktop('1280px');
   const isMobileSize = useIsMobile('899px');
   const deviceType = useDeviceType();
@@ -20,7 +26,9 @@ export default function RecentProjectListCarousel({ children }: { children: JSX.
       rightArrowType={arrowType}
       overflowType={overflowType}
     >
-      {children}
+      {recentProjectList.slice(0, 6).map((project) => (
+        <RecentProjectListItem key={project.id} {...project} />
+      ))}
     </Carousel>
   );
 }
