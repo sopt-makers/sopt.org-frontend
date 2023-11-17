b15d6be204676c680cbf882e5bff8ca609dc5803
f0ever0 <thddndud0830@gmail.com>
2023-11-17 20:42:11 +0900

    feat : scale 비율 조정

--- src/views/MainPage/components/Introduce/index.tsx
+++ src/views/MainPage/components/Introduce/index.tsx
@@ -16,7 +16,7 @@ export default function Introduce() {
   const opacityValue = useTransform(scrollYProgress, [1, 0.4], ['0%', '100%']);
 
   const clipPathValue = useMotionTemplate`inset(0% ${scrollValue} 0% 0%)`;
-  const scaleValue = useTransform(scrollYProgress, [1, 0.4], [0.5, 1]);
+  const scaleValue = useTransform(scrollYProgress, [1, 0.4], [0.9, 1]);
 
   const content = '전국 최대 규모의 대학생 IT 연합 동아리, SOPT를 소개합니다.';
 
