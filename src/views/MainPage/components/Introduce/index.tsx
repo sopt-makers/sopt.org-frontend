cee18315d8e2c50ab1a6f8380117ea589e158d85
f0ever0 <thddndud0830@gmail.com>
2023-11-17 21:59:38 +0900

    feat : 모바일 clipPathValue 수정

--- src/views/MainPage/components/Introduce/index.tsx
+++ src/views/MainPage/components/Introduce/index.tsx
@@ -1,24 +1,32 @@
 import { useMotionTemplate, useScroll, useTransform } from 'framer-motion';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useIsMobile } from '@src/hooks/useDevice';
 import * as S from './style';
 
 export default function Introduce() {
   const contentRef = useRef<HTMLElement>(null);
   const isMobileSize = useIsMobile('768px');
-
   const { scrollYProgress } = useScroll({
     target: contentRef,
     offset: ['end center', 'start start'],
   });
+  const [clipPathValue, setClipPathValue] = useState<string>('inset(0% 0% 0% 0%)');
 
   const scrollValue = useTransform(scrollYProgress, [1, 0.4], ['100%', '0%']);
   const opacityValue = useTransform(scrollYProgress, [1, 0.4], ['0%', '100%']);
-
-  const clipPathValue = useMotionTemplate`inset(0% ${scrollValue} 0% 0%)`;
   const scaleValue = useTransform(scrollYProgress, [1, 0.4], [0.9, 1]);
 
   const content = '전국 최대 규모의 대학생 IT 연합 동아리, SOPT를 소개합니다.';
+
+  useEffect(() => {
+    const unsubscribe = scrollValue.on('change', (value) => {
+      setClipPathValue(`inset(0% ${value} 0% 0%)`);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [scrollValue]);
 
   return (
     <S.Background ref={contentRef}>
