8fde1a17b790f951ab75c513c7737a672f5ee85e
solar3070 <>
2023-11-21 00:43:36 +0900

    style: 태블릿, 모바일 반응형

--- src/views/MainPage/components/IntroSection/IntroContent/style.ts
+++ src/views/MainPage/components/IntroSection/IntroContent/style.ts
@@ -39,6 +39,7 @@ export const MotionSvg = styled(motion.svg)<{ top: number }>`
 
 export const IntroContent = styled.div`
   display: flex;
+  justify-content: center;
   align-items: center;
   position: relative;
   width: 100vw;
@@ -48,7 +49,6 @@ export const IntroContent = styled.div`
 
 export const ContentWithCircle = styled(motion.div)`
   position: relative;
-  margin-left: 370px;
 `;
 
 export const Circle = styled.div`
@@ -57,7 +57,7 @@ export const Circle = styled.div`
   align-items: center;
 
   position: absolute;
-  top: -48px;
+  top: -148px;
   left: -410.91px;
   z-index: 30;
 
@@ -72,6 +72,27 @@ export const Circle = styled.div`
     rgba(46, 57, 60, 0.35) 0%,
     rgba(7, 9, 12, 0) 100%
   );
+
+  & > svg {
+    width: 130px;
+    height: 130px;
+  }
+
+  @media (max-width: 1440px) and (min-width: 769px) {
+    grid-template-rows: auto 1fr;
+    grid-gap: 24px 0px;
+    top: -158px;
+  }
+
+  @media (max-width: 768px) {
+    top: 80.19px;
+    left: -345.83px;
+
+    & > svg {
+      width: 100px;
+      height: 100px;
+    }
+  }
 `;
 
 export const Content = styled(motion.div)`
@@ -79,12 +100,27 @@ export const Content = styled(motion.div)`
   grid-template-areas:
     'title image'
     'desc image';
-  grid-template-rows: 158px 1fr;
+  grid-template-rows: auto 1fr;
   grid-gap: 24px 57px;
 
   position: relative;
   z-index: 40;
-  margin-top: 100px;
+
+  @media (max-width: 1440px) and (min-width: 769px) {
+    grid-template-rows: auto 1fr;
+    grid-gap: 24px 0px;
+  }
+
+  @media (max-width: 768px) {
+    grid-template-areas:
+      'image'
+      'title'
+      'desc';
+    grid-template-rows: auto auto 1fr;
+    grid-gap: 16px;
+
+    font-size: 28px;
+  }
 `;
 
 export const ContentTitle = styled(motion.h2)`
@@ -103,6 +139,18 @@ export const ContentTitle = styled(motion.h2)`
   font-weight: 700;
   line-height: normal;
   white-space: pre-wrap;
+
+  @media (max-width: 1440px) and (min-width: 769px) {
+    margin-top: 28px;
+    font-size: 40px;
+  }
+
+  @media (max-width: 768px) {
+    width: fit-content;
+    margin-top: 48px;
+    margin-left: 58px;
+    font-size: 28px;
+  }
 `;
 
 export const ContentDetail = styled(motion.h3)`
@@ -117,21 +165,51 @@ export const ContentDetail = styled(motion.h3)`
   line-height: 160%; /* 38.4px */
   letter-spacing: -0.48px;
   white-space: pre-wrap;
+
+  @media (max-width: 1440px) and (min-width: 769px) {
+    font-size: 20px;
+    letter-spacing: -0.4px;
+  }
+
+  @media (max-width: 768px) {
+    width: 259px;
+    margin-left: 58px;
+    font-size: 16px;
+    letter-spacing: -0.32px;
+    white-space: normal;
+  }
 `;
 
 export const ContentImage = styled(Image)`
   grid-area: image;
   border-radius: 50px;
+
+  @media (max-width: 1440px) and (min-width: 769px) {
+    width: 540px;
+    height: 360px;
+  }
+
+  @media (max-width: 768px) {
+    width: 323px;
+    height: 215px;
+    border-radius: 25px;
+    box-shadow: 0px 0px 64px 0px rgba(15, 16, 18, 0.9);
+  }
 `;
 
 export const BackLight = styled(motion.div)`
   position: absolute;
   top: -114.94px;
-  left: 385.37px;
+  left: calc(100vw - 80%);
   z-index: -99;
 
   width: 1954px;
   height: 1084px;
   transform: rotate(7.639deg);
+  flex-shrink: 0;
   background: radial-gradient(45.16% 45.16% at 50% 50%, #2c3242 0%, rgba(15, 15, 18, 0) 100%);
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
