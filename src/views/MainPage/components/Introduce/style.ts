1d7970d9d45397ebffba172f41281847d248b999
wooyoung <62867581+f0rever0@users.noreply.github.com>
2023-11-22 15:37:16 +0900

    [SP2] 메인페이지 상단 두번째 섹션 애니메이션 (#288)
    * feat : text scroll animation
    
    * feat : 모바일 애니메이션 변경
    
    * feat : scale 비율 조정
    
    * feat : content margin 수정
    
    * fix : 안쓰는 style 삭제
    
    * fix : content default opacity 삭제
    
    * feat : 모바일 clipPathValue 수정
    
    * chore : 안쓰는 import 삭제
    
    * chore : Introduce 코드 구조 변경
    
    * feat : 모바일 max-width 변경
    
    * chore : MotionTitle 하나의 style로 관리
    
    * chore : 컴포넌트 명 대소문자 수정

--- src/views/MainPage/components/Introduce/style.ts
+++ src/views/MainPage/components/Introduce/style.ts
@@ -1,13 +1,15 @@
 import styled from '@emotion/styled';
 import { colors } from '@sopt-makers/colors';
+import { motion } from 'framer-motion';
 
 export const Background = styled.section`
   display: flex;
   max-width: 1920px;
-  height: 100vh;
+  height: 200vh;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   background: ${colors.gray950};
+  position: relative;
 
   @media (max-width: 768px) and (min-width: 376px) {
     width: 100vw;
@@ -22,19 +24,28 @@ export const Background = styled.section`
   }
 `;
 
-export const Title = styled.h1`
+export const Wrapper = styled.section`
+  height: 100vh;
+  top: 0;
+  position: -webkit-sticky;
+  position: sticky;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+export const TextContainer = styled(motion.span)`
+  position: relative;
+  overflow: hidden;
+  display: block;
   text-align: center;
   font-size: 50px;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-  background: linear-gradient(93deg, #c9d8dd -34.77%, #fff -20.17%, #636770 6.12%);
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
 
-  /* 모바일 뷰 */
-  @media (max-width: 768px) and (min-width: 376px) {
+  @media (max-width: 768px) and (min-width: 433px) {
     font-size: 28px;
     width: 433px;
     word-break: keep-all;
@@ -43,10 +54,18 @@ export const Title = styled.h1`
   }
 
   /* 모바일 뷰 */
-  @media (max-width: 375px) {
+  @media (max-width: 432px) {
     font-size: 28px;
     width: 250px;
     word-break: keep-all;
     text-align: left;
+    margin: auto;
   }
+`;
+
+export const BackgroundTitle = styled.span`
+  background: linear-gradient(93deg, #c9d8dd -34.77%, #fff -14.77%, #636770 6.12%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 `;
