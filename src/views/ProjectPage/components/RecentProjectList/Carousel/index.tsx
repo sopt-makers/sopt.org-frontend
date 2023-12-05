ad4ca665e8b5aea2725139d9fdb83cfe31a4b108
solar3070 <>
2023-12-06 00:04:34 +0900

    fix: 정적 데이터만 보여주도록 수정

--- src/views/ProjectPage/components/RecentProjectList/Carousel/index.tsx
+++ src/views/ProjectPage/components/RecentProjectList/Carousel/index.tsx
@@ -1,18 +1,9 @@
 import Carousel from '@src/components/common/Carousel';
 import { useDeviceType, useIsDesktop, useIsMobile } from '@src/hooks/useDevice';
 import { staticReleaseProjectList } from '@src/lib/constants/project';
-import { ProjectCategoryType, ProjectPlatformType, ProjectType } from '@src/lib/types/project';
 import RecentProjectListItem from '@src/views/ProjectPage/components/RecentProjectList/Item';
-import { useGetProjectList } from '@src/views/ProjectPage/hooks/queries';
 
 export default function RecentProjectListCarousel() {
-  const { data } = useGetProjectList(ProjectCategoryType.ALL, ProjectPlatformType.ALL);
-  const platform = ['website', 'appStore', 'googlePlay'];
-  const filteredData = (data as ProjectType[])
-    .filter(({ link }) => link.some(({ title }) => platform.includes(title)))
-    .slice(0, 4);
-  const recentProjectList = [...staticReleaseProjectList, ...filteredData];
-
   const isDesktopSize = useIsDesktop('1280px');
   const isMobileSize = useIsMobile('899px');
   const deviceType = useDeviceType();
@@ -26,7 +17,7 @@ export default function RecentProjectListCarousel() {
       gapWidth={isMobileSize ? 14 : 24}
       isDesktop={isDesktop}
     >
-      {recentProjectList.map((project, index) => (
+      {staticReleaseProjectList.slice(0, 6).map((project, index) => (
         <RecentProjectListItem key={index} {...project} />
       ))}
     </Carousel>
