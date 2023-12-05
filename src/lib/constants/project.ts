80126dbe7ed16b79f865e8d0a1552c3116869a69
Hyejun Lee <solar3070@naver.com>
2023-12-06 00:41:55 +0900

    [SP2] 최근 출시된 프로젝트 추가 (#310)
    * asset: 프로젝트 로고 이미지 추가
    
    * feat: 최근 출시된 프로젝트 정적 데이터 추가
    
    * feat: 디바이스 타입에 맞는 링크 보여주기
    
    * feat: 링크가 있는 프로젝트만 보여주기
    
    * chore: 코드 리뷰 반영
    
    * fix: 정적 데이터만 보여주도록 수정
    
    * fix: 데이터 필터링 코드 제거
    
    ---------
    
    Co-authored-by: solar3070 <>

--- src/lib/constants/project.ts
+++ src/lib/constants/project.ts
@@ -1,4 +1,11 @@
-import { ProjectCategoryType, ProjectPlatformType } from '@src/lib/types/project';
+import { default as ImgProjectLogo1 } from '@src/assets/images/recent-release-project/img_project_logo1.avif';
+import { default as ImgProjectLogo2 } from '@src/assets/images/recent-release-project/img_project_logo2.avif';
+import {
+  LinkType,
+  ProjectCategoryType,
+  ProjectPlatformType,
+  StaticProjectType,
+} from '@src/lib/types/project';
 import { PageType } from '@src/lib/types/universal';
 
 export const activeProjectCategoryList: ProjectCategoryType[] = [
@@ -41,3 +48,35 @@ export const pageBreakPoint: Record<PageType, string> = {
   [PageType.BLOG]: '767px',
   [PageType.PROJECT]: '899px',
 };
+
+export const staticReleaseProjectList: StaticProjectType[] = [
+  {
+    name: '엄빠도 어렸다',
+    summary: '긍정적인 소비 습관을 만드는 절약 SNS',
+    generation: 32,
+    link: [
+      {
+        title: LinkType['앱 스토어'],
+        url: 'https://apps.apple.com/kr/app/%EC%9C%84%EB%8B%88-%EC%89%BD%EA%B3%A0-%EC%9E%AC%EB%B0%8C%EB%8A%94-%EA%B8%8D%EC%A0%95%EC%A0%81-%EC%86%8C%EB%B9%84%EC%8A%B5%EA%B4%80-%ED%98%95%EC%84%B1/id6463294662',
+      },
+      {
+        title: LinkType['구글 플레이스토어'],
+        url: 'https://play.google.com/store/apps/details?id=org.go.sopt.winey&hl=ko',
+      },
+      
+    ],
+    logoImage: ImgProjectLogo1.src,
+  },
+  {
+    name: '위니',
+    summary: '긍정적인 소비 습관을 만드는 절약 SNS',
+    generation: 32,
+    link: [
+      {
+        title: LinkType['앱 스토어'],
+        url: 'https://apps.apple.com/kr/app/%EC%9C%84%EB%8B%88-%EC%89%BD%EA%B3%A0-%EC%9E%AC%EB%B0%8C%EB%8A%94-%EA%B8%8D%EC%A0%95%EC%A0%81-%EC%86%8C%EB%B9%84%EC%8A%B5%EA%B4%80-%ED%98%95%EC%84%B1/id6463294662',
+      },
+    ],
+    logoImage: ImgProjectLogo2.src,
+  },
+];
