b3a5f0fbc759ab3e68b8d99d2776f34d90810793
f0ever0 <thddndud0830@gmail.com>
2023-12-13 11:04:57 +0900

    feat : 리쿠르팅 메시지

--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -5,6 +5,7 @@ import Activity from './components/Activity';
 import Banner from './components/Banner';
 import Introduce from './components/Introduce';
 import OwnOrganization from './components/OwnOrganization';
+import RecruitMessage from './components/RecruitMessage';
 import ScrollInteractiveLogo from './components/ScrollInteractiveLogo';
 
 function MainPage() {
@@ -17,6 +18,7 @@ function MainPage() {
       <Activity />
       <PartConfig />
       <OwnOrganization />
+      <RecruitMessage />
     </PageLayout>
   );
 }
---
+++ src/views/MainPage/components/RecruitMessage/index.tsx
@@ -0,0 +1,16 @@
+import { useIsMobile } from '@src/hooks/useDevice';
+import * as S from './style';
+
+export default function RecruitMessage() {
+  const isMobileSize = useIsMobile('768px');
+
+  return (
+    <S.Background>
+      <S.Title>SOPT의 34번째 {isMobileSize && <br />} 열정이 되어주세요!</S.Title>
+      <S.Description>
+        아직 모집기간이 아니예요. {isMobileSize && <br />} 알림 신청을 하시면, 봄에 찾아갈게요!
+      </S.Description>
+      <S.GoToRecruit href="/recruit">모집시 알림 받기</S.GoToRecruit>
+    </S.Background>
+  );
+}
