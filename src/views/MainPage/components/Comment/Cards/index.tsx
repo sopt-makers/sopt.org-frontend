2066b8fcf6d4083d98fc8289fefbd4af55219d87
SeojinSeojin <1106laura@naver.com>
2023-12-21 17:21:12 +0900

    feat: implement mobile

--- src/views/MainPage/components/Comment/Cards/index.tsx
+++ src/views/MainPage/components/Comment/Cards/index.tsx
@@ -1,40 +1,26 @@
-import Carousel from '@src/components/common/Carousel';
 import { useIsDesktop } from '@src/hooks/useDevice';
 import { SOPT_COMMENT_LIST } from '@src/lib/constants/main';
-import CommentCard from '../Card';
+import CommentCardDesktop from '../Card/Desktop';
+import CommentCardMobile from '../Card/Mobile';
 import * as S from '../style';
 
-type CommentCardProps = {
+type CommentCardsProps = {
   activeIdx: number;
   changeActiveIdx: (idx: number) => void;
 };
 
-export default function CommentCards({ activeIdx, changeActiveIdx }: CommentCardProps) {
+export default function CommentCards({ activeIdx, changeActiveIdx }: CommentCardsProps) {
   const isDesktop = useIsDesktop('768px');
   if (isDesktop)
     return <CommentCardsDesktop activeIdx={activeIdx} changeActiveIdx={changeActiveIdx} />;
-  return (
-    <Carousel itemWidth={516} gapWidth={0} stride={1}>
-      {SOPT_COMMENT_LIST.map((comment, idx) => (
-        <CommentCard
-          key={idx}
-          idx={idx}
-          isActive={activeIdx === idx}
-          setIsActive={changeActiveIdx}
-          color={comment.color}
-          comment={comment.comment}
-          commenter={comment.commenter}
-        />
-      ))}
-    </Carousel>
-  );
+  return <CommentCardsMobile activeIdx={activeIdx} changeActiveIdx={changeActiveIdx} />;
 }
 
-function CommentCardsDesktop({ activeIdx, changeActiveIdx }: CommentCardProps) {
+function CommentCardsDesktop({ activeIdx, changeActiveIdx }: CommentCardsProps) {
   return (
     <S.CardWrapper>
       {SOPT_COMMENT_LIST.map((comment, idx) => (
-        <CommentCard
+        <CommentCardDesktop
           key={idx}
           idx={idx}
           isActive={activeIdx === idx}
@@ -44,6 +30,20 @@ function CommentCardsDesktop({ activeIdx, changeActiveIdx }: CommentCardProps) {
           commenter={comment.commenter}
         />
       ))}
+    </S.CardWrapper>
+  );
+}
+
+function CommentCardsMobile({ activeIdx, changeActiveIdx }: CommentCardsProps) {
+  const activeCard = SOPT_COMMENT_LIST[activeIdx];
+  const commentLength = SOPT_COMMENT_LIST.length;
+  return (
+    <S.CardWrapper>
+      <S.LeftArrow
+        onClick={() => changeActiveIdx((activeIdx - 1 + commentLength) % commentLength)}
+      />
+      <CommentCardMobile {...activeCard} />
+      <S.RightArrow onClick={() => changeActiveIdx((activeIdx + 1) % commentLength)} />
     </S.CardWrapper>
   );
 }
--- src/views/MainPage/components/Comment/style.ts
+++ src/views/MainPage/components/Comment/style.ts
@@ -1,11 +1,19 @@
 import styled from '@emotion/styled';
 import { colors } from '@sopt-makers/colors';
 import { motion } from 'framer-motion';
+import icLeftArrow from '@src/assets/icons/ic_arrow_left.svg';
+import icRightArrow from '@src/assets/icons/ic_arrow_right.svg';
 
 export const Wrapper = styled.div`
   position: relative;
   padding-top: 200px;
   padding-bottom: 800px;
+  @media (max-width: 768px) {
+    height: 130vh;
+  }
+  @media (max-width: 520px) {
+    height: inherit;
+  }
 `;
 
 export const TitleWrapper = styled.div<{ activeIdx: number }>`
@@ -20,6 +28,11 @@ export const TitleWrapper = styled.div<{ activeIdx: number }>`
     padding-bottom: 80px;
     transition: 0.4s;
   }
+  @media (max-width: 768px) {
+    position: static;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
 `;
 
 export const CardWrapper = styled.div`
@@ -29,6 +42,17 @@ export const CardWrapper = styled.div`
   @media (max-width: 1440px) {
     padding-top: 200px;
     padding-bottom: 600px;
+  }
+  @media (max-width: 768px) {
+    padding-top: 64px;
+    flex-direction: row;
+    align-items: center;
+    gap: 22px;
+  }
+  @media (max-width: 520px) {
+    padding-top: 32px;
+    padding-bottom: 0;
+    gap: 12px;
   }
 `;
 
@@ -44,11 +68,17 @@ export const SectionTitle = styled.div`
     padding-bottom: 200px;
   }
   @media (max-width: 768px) {
-    padding-top: 200px;
     font-family: SUIT;
     font-size: 41px;
     line-height: 45.956px; /* 112.087% */
     letter-spacing: -1.23px;
+  }
+  @media (max-width: 520px) {
+    font-size: 21px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 24.28px; /* 115.617% */
+    letter-spacing: -0.63px;
   }
 `;
 
@@ -66,7 +96,40 @@ export const Summary = styled(motion.div)<{ color: string }>`
   @media (max-width: 768px) {
     font-size: 57px;
     font-weight: 600;
-    line-height: 45.956px; /* 80.624% */
+    line-height: 60px;
     letter-spacing: -1.71px;
+    height: 156px;
   }
+  @media (max-width: 520px) {
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 32px;
+    letter-spacing: -0.9px;
+    height: 120px;
+  }
+`;
+
+const Arrow = styled.div`
+  width: 26px;
+  height: 42px;
+  background-color: #b1c3d9b2;
+  flex-shrink: 0;
+  cursor: pointer;
+  mask-repeat: no-repeat;
+  mask-size: contain;
+  mask-position: center;
+
+  @media (max-width: 520px) {
+    width: 15px;
+    height: 24px;
+  }
+`;
+
+export const LeftArrow = styled(Arrow)`
+  mask-image: url(${icLeftArrow});
+`;
+
+export const RightArrow = styled(Arrow)`
+  mask-image: url(${icRightArrow});
 `;
