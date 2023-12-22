43c70715c6996282b6dc79cc94266941746219ee
f0ever0 <thddndud0830@gmail.com>
2023-12-22 12:22:17 +0900

    feat : 배너 화살표 버튼

--- src/views/MainPage/components/Banner/style.ts
+++ src/views/MainPage/components/Banner/style.ts
@@ -48,6 +48,7 @@ export const ContentWrapper = styled.article`
   align-items: center;
   justify-content: space-between;
   width: 100vw;
+  height: 100vh;
   padding-top: 50vh;
   position: absolute;
   z-index: 2;
@@ -57,7 +58,6 @@ export const ContentWrapper = styled.article`
   @media (max-width: 768px) {
     padding-top: 450px;
     padding-bottom: 50px;
-    gap: 20px;
   }
 `;
 
