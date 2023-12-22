0635ba2a0e3a297ebcc8052cd14ec80f9d0ed4f0
f0ever0 <thddndud0830@gmail.com>
2023-12-22 12:06:09 +0900

    feat : 배너 화살표 버튼 보이기

--- src/views/MainPage/components/Banner/style.ts
+++ src/views/MainPage/components/Banner/style.ts
@@ -48,7 +48,6 @@ export const ContentWrapper = styled.article`
   align-items: center;
   justify-content: space-between;
   width: 100vw;
-  height: 100vh;
   padding-top: 50vh;
   position: absolute;
   z-index: 2;
@@ -58,6 +57,7 @@ export const ContentWrapper = styled.article`
   @media (max-width: 768px) {
     padding-top: 60vh;
     padding-bottom: 50px;
+    gap: 20px;
   }
 `;
 
