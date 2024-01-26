56efb0f097d8d3dbc8d38593896a22ba1e13eccf
SeojinSeojin <1106laura@naver.com>
2024-01-26 15:30:23 +0900

    Squashed commit of the following:
    commit 3dff4d2ec8663f3e41535cac8c7ba14e071122f7
    Author: f0ever0 <thddndud0830@gmail.com>
    Date:   Thu Jan 11 23:13:55 2024 +0900
    
        chore : 오타 수정
    
    commit 2629d82fb4b95b0e58a68948c2836783228f6455
    Author: f0ever0 <thddndud0830@gmail.com>
    Date:   Wed Jan 10 21:17:15 2024 +0900
    
        feat : 후원 문의하기 버튼
    
    commit 3ef32d715c342e7a9f81236b2b7d9406233382f8
    Author: f0ever0 <thddndud0830@gmail.com>
    Date:   Wed Jan 10 21:11:03 2024 +0900
    
        feat : sponsor layout 작업
    
    commit 16d11cbaa4f5f47be1d2b99c3c958daed4c9ed8a
    Author: f0ever0 <thddndud0830@gmail.com>
    Date:   Wed Jan 10 20:58:53 2024 +0900
    
        chore : 안쓰는 컴포넌트 삭제

--- src/views/SponsorPage/components/index.ts
+++ src/views/SponsorPage/components/index.ts
@@ -1,3 +1,1 @@
-export { default as CorporateJob } from './CorporateJob';
-export { default as CorporateLinkedActivities } from './CorporateLinkedActivities';
 export { default as CorporatePartner } from './CorporatePartner';
