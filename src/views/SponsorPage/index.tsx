8259c2e5834417402baae34d017dc424017f6b7a
wooyoung <62867581+f0rever0@users.noreply.github.com>
2024-01-28 13:17:50 +0900

    [SP4] 후원탭 개선작업 (#349)
    * chore : 안쓰는 컴포넌트 삭제
    
    * feat : sponsor layout 작업
    
    * feat : 후원 문의하기 버튼
    
    * chore : 오타 수정

--- src/views/SponsorPage/index.tsx
+++ src/views/SponsorPage/index.tsx
@@ -1,55 +1,12 @@
-import styled from '@emotion/styled';
 import PageLayout from '@src/components/common/PageLayout';
-import { CorporateJob, CorporateLinkedActivities, CorporatePartner } from './components';
+import { CorporatePartner } from './components';
 
 const SponsorPage = () => {
   return (
     <PageLayout>
-      <Container>
-        <Content>
-          <CorporateJob />
-          <CorporatePartner />
-          <CorporateLinkedActivities />
-        </Content>
-      </Container>
+      <CorporatePartner />
     </PageLayout>
   );
 };
 
 export default SponsorPage;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  position: relative;
-  padding-top: 180px;
-
-  /* 태블릿 뷰 */
-  @media (max-width: 1199px) and (min-width: 766px) {
-    padding-top: 150px;
-  }
-  /* 모바일 뷰 */
-  @media (max-width: 765.9px) {
-    padding-top: 87px;
-  }
-`;
-
-const Content = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  width: 1200px;
-  gap: 190px;
-
-  /* 태블릿 뷰 */
-  @media (max-width: 1199px) and (min-width: 766px) {
-    max-width: 766px;
-    gap: 150px;
-  }
-  /* 모바일 뷰 */
-  @media (max-width: 765.9px) {
-    max-width: 360px;
-    gap: 100px;
-  }
-`;
