e37fe6846621c50de267d5dab031bd7e7a735b5a
SeojinSeojin <1106laura@naver.com>
2024-01-28 17:41:31 +0900

    hotfix: 기수 이름 바꾸기

--- src/views/AboutPage/components/Record/Section/index.tsx
+++ src/views/AboutPage/components/Record/Section/index.tsx
@@ -8,7 +8,7 @@ type RecordSectionProps = Pick<AboutInfoType, 'generation' | 'records'>;
 const RecordSection = (props: RecordSectionProps) => {
   return (
     <St.Wrapper>
-      <SectionTop engTitle="Activity Records" korTitle={`${props.generation - 1}기 활동 레코드`} />
+      <SectionTop engTitle="Activity Records" korTitle={`${props.generation}기 활동 레코드`} />
       <RecordList records={props.records} />
     </St.Wrapper>
   );
