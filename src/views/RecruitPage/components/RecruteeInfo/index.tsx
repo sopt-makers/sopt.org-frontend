ed9223f6098a1b880217aa5aa26b602851b799dd
lydiacho <sheecho0804@ewhain.net>
2024-03-01 01:42:55 +0900

    design: RecruiteeInfo SubTitle 추가

--- src/views/RecruitPage/components/RecruteeInfo/index.tsx
+++ src/views/RecruitPage/components/RecruteeInfo/index.tsx
@@ -1,6 +1,7 @@
-import styled from '@emotion/styled';
+import * as S from './style';
 import {
   SectionDesc,
+  SectionSubTitle,
   SectionTitle,
   SectionTitleTranslate,
   SectionTitleWrapper,
@@ -8,30 +9,17 @@ import {
 
 const RecruiteeInfo = () => {
   return (
-    <Wrapper>
+    <S.Wrapper>
       <SectionTitleWrapper>
         <SectionTitleTranslate>Recruitment target</SectionTitleTranslate>
         <SectionTitle>모집 대상</SectionTitle>
       </SectionTitleWrapper>
+      <SectionSubTitle>아래 3가지 모두 해당 되는 분이라면, 누구든 지원 가능해요!</SectionSubTitle>
       <SectionDesc>
         IT 창업 및 웹/앱 서비스에 관심이 많고, 수도권 내에서 활동이 가능한 열정적인 대학생
       </SectionDesc>
-    </Wrapper>
+    </S.Wrapper>
   );
 };
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 28px;
-  /* 태블릿 뷰 */
-  @media (max-width: 1299px) and (min-width: 766px) {
-    gap: 40px;
-  }
-  /* 모바일 뷰 */
-  @media (max-width: 765.9px) {
-    gap: 8px;
-  }
-`;
 
 export default RecruiteeInfo;
---
+++ src/views/RecruitPage/components/RecruteeInfo/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  /* 태블릿 뷰 */
+  @media (max-width: 1299px) and (min-width: 766px) {
+  }
+  /* 모바일 뷰 */
+  @media (max-width: 765.9px) {
+  }
+`;
