86c5e502ec8a43a44e2f9194c03d00f5aa6c71b6
lydiacho <sheecho0804@ewhain.net>
2024-03-01 23:49:00 +0900

    feat: 지정 시각부터 배너 띄우기

--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -6,12 +6,17 @@ import Introduce from './components/Introduce';
 import ScrollInteractiveLogo from './components/ScrollInteractiveLogo';
 import TopBanner from './components/TopBanner';
 import usePost from './hooks/usePost';
+import useCheckTime from './hooks/useCheckTime';
 
 function MainPage() {
+  const START_DATE = new Date('2024-03-02:01:00');
+  const isStarted = useCheckTime(START_DATE); 
+
   usePost();  // 방문자 증가
+  
   return (
     <PageLayout>
-      <TopBanner/>
+      {isStarted && <TopBanner/>}
       <Banner />
       <Introduce />
       <IntroSection />
