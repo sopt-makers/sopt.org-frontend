581c7951e10bb970ccd7052f1d91eb93468a9d22
lydiacho <sheecho0804@ewhain.net>
2024-03-08 17:13:59 +0900

    fix: 모집 마감 시간 픽스

--- src/hooks/useCheckTime.ts
+++ src/hooks/useCheckTime.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 export default function useCheckTime() {
   const START_DATE = new Date('2024-03-03 00:00:00');
-  const END_DATE = new Date('2024-03-08 17:00:00');
+  const END_DATE = new Date('2024-03-08 18:00:00');
   const [startDiff, setStartDiff] = useState<number>(START_DATE.getTime() - Date.now());
   const [endDiff, setEndDiff] = useState<number>(Date.now() - END_DATE.getTime());
   
