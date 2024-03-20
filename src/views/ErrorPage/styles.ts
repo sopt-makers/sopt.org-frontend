4773b42c13180d1aecc05e1c2cefb14c65b31079
lydiacho <sheecho0804@ewhain.net>
2024-03-20 23:56:58 +0900

    design: 문의하기 버튼 추가

--- src/views/ErrorPage/styles.ts
+++ src/views/ErrorPage/styles.ts
@@ -1,14 +1,26 @@
 import styled from '@emotion/styled';
 
-export const Root = styled.section`
+export const Root = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 22px;
+
+  position: relative;
 
   width: 100%;
   height: 100vh;
+`;
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 22px;
+
+  position: absolute;
+  top: 268px;
+
+  width: 100%;
 `;
 export const ErrorText = styled.p`
   margin-bottom: 26px;
@@ -23,4 +35,17 @@ export const ErrorText = styled.p`
 export const CodeText = styled.div`
   display: flex;
   align-items: center;
+`;
+export const ContactButton = styled.button`
+  position: absolute;
+  bottom: 169px;
+
+  color: #FFF;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 150%; /* 36px */
+  letter-spacing: -0.48px;
+  text-decoration-line: underline;
+
+  cursor: pointer;
 `;
