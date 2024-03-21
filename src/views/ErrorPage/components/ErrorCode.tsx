778c9d523fd630317e1bcb37bb43ea6a26a69064
lydiacho <sheecho0804@ewhain.net>
2024-03-21 14:00:34 +0900

    feat: 에러코드 컴포넌트 반응형 작업

--- src/views/ErrorPage/components/ErrorCode.tsx
+++ src/views/ErrorPage/components/ErrorCode.tsx
@@ -1,20 +1,28 @@
-import { Ic404Back, Ic404Front, Ic404Ghost, Ic500Back, Ic500Cone, Ic500Front } from '../assets';
+import { useIsMobile, useIsTablet } from '@src/hooks/useDevice';
+import { Ic404Back, Ic404Front, Ic404Ghost, Ic404GhostDark, Ic500Back, Ic500Cone, Ic500ConeDark, Ic500Front } from '../assets';
 
 interface ErrorCodeProps {
-  code: number;
+  code: 404 | 500;
 }
 export default function ErrorCode({ code }: ErrorCodeProps) {
+  const isMobile = useIsMobile('428px');
+  const isTablet = useIsTablet('428px', '768px');
+  const SIZE = {
+    height: isTablet ? 89 : 101,
+    icon: isTablet ? 140 : 165,
+  };
+
   return code === 404 ? (
     <>
-      <Ic404Front />
-      <Ic404Ghost />
-      <Ic404Back />
+      {!isMobile && <Ic404Front height={SIZE.height} />}
+      {isMobile ? <Ic404GhostDark /> : <Ic404Ghost height={SIZE.icon}/>}
+      {!isMobile && <Ic404Back height={SIZE.height} />}
     </>
   ) : (
     <>
-      <Ic500Front />
-      <Ic500Cone />
-      <Ic500Back />
+        {!isMobile && <Ic500Front height={SIZE.height} />}
+        {isMobile ? <Ic500ConeDark /> : <Ic500Cone height={SIZE.icon}/>}
+        {!isMobile && <Ic500Back height={SIZE.height} />}
     </>
   );
 }
