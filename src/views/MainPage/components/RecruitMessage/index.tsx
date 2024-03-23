e1ab8887afa81c79e2deea798ec18099cfa73a91
Jeon Eonseok <jseokeon@gmail.com>
2024-03-23 10:44:56 +0900

    design: recruit message section 적응형 구현

--- src/views/MainPage/components/RecruitMessage/index.tsx
+++ src/views/MainPage/components/RecruitMessage/index.tsx
@@ -1,14 +1,16 @@
 import { useIsMobile } from '@src/hooks/useDevice';
 import * as S from './style';
-import RecruitButton from '../Banner/RecruitButton';
 
 export default function RecruitMessage() {
   const isMobileSize = useIsMobile('768px');
 
   return (
     <S.Background>
-      <S.Title>지금은 모집기간이 아니에요. {isMobileSize && <br />} 모집 기간이 되면 메일로 알려드릴게요.</S.Title>
-      <RecruitButton>모집 알림 신청하기 &gt; </RecruitButton>
+      <S.Title>솝트의 34번째 열정{isMobileSize && <br />}이 되어주세요!</S.Title>
+      <S.Description>
+        아직 모집기간이 아니예요.{isMobileSize && <br />}알림 신청을 하시면, 봄에 찾아갈게요!
+      </S.Description>
+      <S.GoToRecruit>모집시 알림 받기</S.GoToRecruit>
     </S.Background>
   );
 }
