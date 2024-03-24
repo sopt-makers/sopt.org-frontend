abce867cc511fa639664058f1bff8c2584cf27d5
Jeon Eonseok <jseokeon@gmail.com>
2024-03-24 11:57:52 +0900

    chore: 워딩 수정

--- src/views/MainPage/components/RecruitMessage/index.tsx
+++ src/views/MainPage/components/RecruitMessage/index.tsx
@@ -6,9 +6,9 @@ export default function RecruitMessage() {
 
   return (
     <S.Background>
-      <S.Title>솝트의 34번째 열정{isMobileSize && <br />}이 되어주세요!</S.Title>
+      <S.Title>솝트의 35번째 열정{isMobileSize && <br />}이 되어주세요!</S.Title>
       <S.Description>
-        아직 모집기간이 아니예요.{isMobileSize && <br />}알림 신청을 하시면, 봄에 찾아갈게요!
+        아직 모집기간이 아니예요.{isMobileSize && <br />}알림 신청을 하시면, 가을에 찾아갈게요!
       </S.Description>
       <S.GoToRecruit>모집시 알림 받기</S.GoToRecruit>
     </S.Background>
