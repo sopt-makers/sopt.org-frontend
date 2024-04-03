c734984b53937334bf664950e281ff9d3af892fb
lydiacho <sheecho0804@ewhain.net>
2024-04-03 17:16:34 +0900

    fix: scss -> emotion으로 마이그레이션

--- src/components/common/ScrollToTopButton/style.ts
+++ src/components/common/ScrollToTopButton/style.ts
@@ -1,13 +1,15 @@
-@import '@src/lib/styles/mixins';
+import styled from '@emotion/styled';
+import { FadeIn } from '@src/lib/styles/animation';
 
-.wrapper {
+export const Wrapper = styled.button`
   position: fixed;
   z-index: 9999;
   cursor: pointer;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
   background: #242424;
 
-  @include mobile {
+  // 모바일
+  @media (max-width: 765px) {
     width: 56px;
     height: 56px;
     right: 20px;
@@ -15,7 +17,8 @@
     border-radius: 50%;
   }
 
-  @include tablet {
+  // 태블릿
+  @media (min-width: 766px) and (max-width: 1919px) {
     width: 70px;
     height: 70px;
     border-radius: 50%;
@@ -23,7 +26,8 @@
     bottom: 120px;
   }
 
-  @include desktop {
+  // 데스크탑
+  @media (min-width: 1920px) {
     width: 123px;
     height: 80px;
     border-radius: 52px;
@@ -35,51 +39,17 @@
     padding: 16px 32px;
   }
 
+  ${FadeIn}
   animation: fadein 0.4s;
-}
+`;
 
-.text {
+export const Text = styled.span`
   font-weight: 600;
   font-size: 24px;
   line-height: 48px;
   color: #ffffff;
-  @include mobile-tablet {
+  // 모바일 태블릿
+  @media (min-width: 360px) and (max-width: 1919px) {
     display: none;
   }
-}
-
-@keyframes fadein {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-@-moz-keyframes fadein {
-  /* Firefox */
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-@-webkit-keyframes fadein {
-  /* Safari and Chrome */
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-@-o-keyframes fadein {
-  /* Opera */
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
+`;
