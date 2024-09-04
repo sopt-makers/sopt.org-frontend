a8eaca061343c623e7930a3e77af0fa97f5b7c22
wuzoo <wndyd9911@gmail.com>
2024-09-04 17:48:32 +0900

    feat: 상단 TopBanner 추가

--- src/views/MainPage/index.tsx
+++ src/views/MainPage/index.tsx
@@ -1,6 +1,7 @@
 import PageLayout from '@src/components/common/PageLayout';
 import BottomLayout from '@src/views/MainPage/components/BottomLayout';
 import IntroSection from '@src/views/MainPage/components/IntroSection';
+import TopBanner from '@src/views/MainPage/components/TopBanner';
 import Banner from './components/Banner';
 import Introduce from './components/Introduce';
 import ScrollInteractiveLogo from './components/ScrollInteractiveLogo';
@@ -8,6 +9,7 @@ import ScrollInteractiveLogo from './components/ScrollInteractiveLogo';
 function MainPage() {
   return (
     <PageLayout>
+      <TopBanner />
       <Banner />
       <Introduce />
       <IntroSection />
