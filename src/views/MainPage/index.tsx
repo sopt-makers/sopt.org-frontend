d0b9740cd6fc4987be524c5549e722a797c6a635
wuzoo <wndyd9911@gmail.com>
2024-09-05 21:26:41 +0900

    feat: 지원기간 여부에 따라 TopBanner 조건부 렌더링

--- src/views/MainPage/index.tsx
+++ src/views/MainPage/index.tsx
@@ -2,14 +2,17 @@ import PageLayout from '@src/components/common/PageLayout';
 import BottomLayout from '@src/views/MainPage/components/BottomLayout';
 import IntroSection from '@src/views/MainPage/components/IntroSection';
 import TopBanner from '@src/views/MainPage/components/TopBanner';
+import { checkIsTimeInRange } from '../../lib/utils/date';
 import Banner from './components/Banner';
 import Introduce from './components/Introduce';
 import ScrollInteractiveLogo from './components/ScrollInteractiveLogo';
 
 function MainPage() {
+  const isValid = checkIsTimeInRange('2024-09-08 10:00:00', '2024-09-13 18:00:00'); // 모집 여부
+
   return (
     <PageLayout>
-      <TopBanner />
+      {isValid && <TopBanner />}
       <Banner />
       <Introduce />
       <IntroSection />
