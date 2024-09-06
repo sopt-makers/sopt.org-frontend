637e8c0aa2f1be014278bd57e84d01690daaed21
wuzoo <wndyd9911@gmail.com>
2024-09-06 21:43:21 +0900

    feat: Section 컴포넌트에서 members data render (Content 컴포넌트 미사용)

--- src/views/AboutPage/components/Member/Section/style.ts
+++ src/views/AboutPage/components/Member/Section/style.ts
@@ -10,3 +10,43 @@ export const MarginTop = styled.div`
     height: 120px;
   }
 `;
+
+export const CardContainer = styled.div`
+  display: grid;
+
+  grid-template-columns: repeat(3, 1fr);
+  gap: 34px;
+  width: 1200px;
+
+  @media (max-width: 80rem) and (min-width: 73.125rem) {
+    width: calc(100% - 40px);
+  }
+
+  @media (max-width: 73.125rem) and (min-width: 48rem) {
+    grid-template-columns: repeat(2, 1fr);
+    width: 752px;
+  }
+
+  @media (max-width: 48rem) and (min-width: 36.5rem) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 24px;
+    width: 576px;
+  }
+  /* 모바일 뷰 */
+  @media (max-width: 36.5rem) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 15px;
+    width: max(350px, 100% - 40px);
+  }
+`;
+
+export const OvalSpinnerWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  padding-top: 200px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
