fb61a550762bb95d852e4a4ea5c281124c51c9d7
lydiacho <sheecho0804@ewhain.net>
2024-09-08 14:45:21 +0900

    fix: 메인페이지 하단 배너 수정

--- src/views/MainPage/components/RecruitMessage/index.tsx
+++ src/views/MainPage/components/RecruitMessage/index.tsx
@@ -1,17 +1,21 @@
 import { useIsMobile } from '@src/hooks/useDevice';
+import { checkIsTimeInRange } from '@src/lib/utils/date';
 import RecruitButton from '../Banner/RecruitButton';
 import * as S from './style';
 
 export default function RecruitMessage() {
   const isMobileSize = useIsMobile('48rem');
+  const isValid = checkIsTimeInRange('2024-09-08 10:00:00', '2024-09-13 18:00:00'); // 모집 여부
 
   return (
     <S.Background>
       <S.Title>솝트의 35번째{isMobileSize ? <br /> : ' '}열정이 되어주세요!</S.Title>
-      <S.Description>
-        아직 모집기간이 아니예요.{isMobileSize && <br />}알림 신청을 하시면, 가을에 찾아갈게요!
-      </S.Description>
-      <RecruitButton>모집시 알림 받기 &gt;</RecruitButton>
+      {!isValid && (
+        <S.Description>
+          아직 모집기간이 아니예요.{isMobileSize && <br />}알림 신청을 하시면, 가을에 찾아갈게요!
+        </S.Description>
+      )}
+      <RecruitButton>{isValid ? '35기 YB 지원하기 ' : '모집 알림 신청하기 '}&gt; </RecruitButton>
     </S.Background>
   );
 }
