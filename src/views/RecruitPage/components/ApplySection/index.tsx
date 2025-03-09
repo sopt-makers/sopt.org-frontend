67defad7ac06ca3ff1ab84114258a6b3241effe9
kimkyuhong <bobo3766@naver.com>
2025-03-09 17:08:00 +0900

    hotfix: 지원하기 페이지 기수 오류 수정

--- src/views/RecruitPage/components/ApplySection/index.tsx
+++ src/views/RecruitPage/components/ApplySection/index.tsx
@@ -7,7 +7,7 @@ const ApplySection = ({ headerImg }: { headerImg: string }) => {
   return (
     <S.Wrapper imgRecruitBg={headerImg}>
       <S.Title>
-        <span>SOPT의 35번째 열정을&nbsp;</span>
+        <span>SOPT의 36번째 열정을&nbsp;</span>
         <span>기다리고 있어요!</span>
       </S.Title>
       <S.ApplyButton href="https://recruit.sopt.org/" target="_blank" main={'#' + main}>
