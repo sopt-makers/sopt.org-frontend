c4dd53049452062da669f37341d87228dd7f4d77
namdaeun <nde40345@gmail.com>
2025-05-09 02:55:26 +0900

    fix: useStorage 리렌더링 이슈 해결

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { getStorageHandler } from '@src/lib/utils/storageHandler';
 
 export default function useStorage<T>(
@@ -6,21 +6,21 @@ export default function useStorage<T>(
   storageType: 'localStorage' | 'sessionStorage',
   defaultValue: T,
 ): [value: T, setValue: (newValue: T) => void] {
-  const handler = getStorageHandler(storageType);
+  const handler = useCallback(() => getStorageHandler(storageType), [storageType]);
   const [valueInState, setValueInState] = useState<T>(defaultValue);
 
   useEffect(() => {
-    const storageItem = handler.getItem<T>(key);
+    const storageItem = handler().getItem<T>(key);
     if (storageItem !== null) {
       setValueInState(storageItem);
     } else if (defaultValue !== undefined) {
-      handler.setItem(key, defaultValue);
+      handler().setItem(key, defaultValue);
       setValueInState(defaultValue);
     }
   }, [handler, key, defaultValue]);
 
   const setValue = (newValue: T) => {
-    handler.setItem(key, newValue);
+    handler().setItem(key, newValue);
     setValueInState(newValue);
   };
 
