cee6b111b676759c8bdf2473323ba1c098ca2df7
Daeun Nam <96781926+namdaeun@users.noreply.github.com>
2025-05-20 16:32:38 +0900

    [FEAT] SOPT 이야기 공식 영상 연결 (#452)
    * feat: react-youtube 라이브러리 설치
    
    * feat: 유튜브 영상 띄우기
    
    * chore: 필요 없는 코드 제거
    
    * style: 반응형 고려
    
    * chore: 필요 없는 코드 제거
    
    * style: 필요 없는 코드 제거
    
    * chore: solve build error

--- src/views/BlogPage/components/OfficialVideo/style.ts
+++ src/views/BlogPage/components/OfficialVideo/style.ts
@@ -24,17 +24,31 @@ export const VideoWrapper = styled.div`
   }
 `;
 
-export const Video = styled.img`
-  width: 401px;
-  height: auto;
-  object-fit: cover;
+export const Video = styled.div`
+  min-width: 401px;
+  overflow: hidden;
+  border-radius: 17px;
+
+  & > iframe {
+    min-width: 401px;
+    height: 224px;
+  }
 
   @media (max-width: 767px) {
-    width: 320px;
+    height: auto;
+
+    & > iframe {
+      min-width: 320px;
+    }
   }
 
   @media (max-width: 430px) {
-    width: 271px;
+    min-width: 271px;
+    height: auto;
+
+    & > iframe {
+      min-width: 271px;
+    }
   }
 `;
 
