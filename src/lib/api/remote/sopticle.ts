909e68631f7654dd8603b8e719aa9e2f47ebeb36
namdaeun <nde40345@gmail.com>
2025-05-31 17:16:05 +0900

    fix: 안 쓰이는 코드 제거 및 unlike post 분리

--- src/lib/api/remote/sopticle.ts
+++ src/lib/api/remote/sopticle.ts
@@ -1,6 +1,6 @@
 import { BASE_URL, DEFAULT_TIMEOUT } from '@src/lib/constants/client';
 import { BlogPostType, BlogResponse, SortType } from '@src/lib/types/blog';
-import { PostSopticleLikeResponse, SopticleAPI } from '@src/lib/types/sopticle';
+import { SopticleAPI } from '@src/lib/types/sopticle';
 import { getStorageHandler } from '@src/lib/utils/storageHandler';
 import axios from 'axios';
 import { nanoid } from 'nanoid';
@@ -29,6 +29,7 @@ export const getResponse = async (
     currentPage: number;
     hasPrevPage: boolean;
     totalPage: number;
+    isLikedByUser: boolean;
     totalCount: number;
   }>(`/soptstory?${parameter}`, { headers: { 'session-id': sessionId } });
 
@@ -38,34 +39,25 @@ export const getResponse = async (
     currentPage: data.currentPage,
     hasPrevPage: data.hasPrevPage,
     totalPage: data.totalPage,
+    isLikedByUser: data.isLikedByUser,
     totalCount: data.totalCount,
   };
 };
 
-const postSopticleLike = async (
-  sopticleId: number,
-  prevLike: boolean,
-): Promise<PostSopticleLikeResponse> => {
-  const sessionStorageHandler = getStorageHandler('sessionStorage');
-  const sessionId = sessionStorageHandler.getItemOrGenerate('session-id', nanoid);
-  const headers = { 'session-id': sessionId };
-  const response = await client.post(
-    `soptstory/${sopticleId}/${prevLike ? 'unlike' : 'like'}`,
-    {},
-    { headers },
-  );
+const postSopticleLike = async (sopticleId: number): Promise<void> => {
+  const response = await client.post(`soptstory/${sopticleId}/like`, {});
 
-  switch (response.status) {
-    case 200:
-    case 201:
-      return { currentLike: !prevLike, likeChanged: true };
-    case 400:
-    default:
-      return { currentLike: prevLike, likeChanged: false };
-  }
+  return response.data;
+};
+
+const postSopticleUnlike = async (sopticleId: number): Promise<void> => {
+  const response = await client.post(`soptstory/${sopticleId}/unlike`, {});
+
+  return response.data;
 };
 
 export const remoteSopticleAPI: SopticleAPI = {
   getResponse,
   postSopticleLike,
+  postSopticleUnlike,
 };
