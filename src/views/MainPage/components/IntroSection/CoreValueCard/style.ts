e2be052b5cf896d6e604b1cf5906f5964f2bbdb2
kimkyuhong <94050324+rtttr1@users.noreply.github.com>
2026-01-25 18:18:50 +0900

    [Feat] 핵심가치 반응형 인터렉션 구현 (#478)
    * feat: 핵심가치 반응형 인터렉션 구현
    
    * feat: 코드리뷰 반영
    
    * fix: 빌드 에러 해결

---
+++ src/views/MainPage/components/IntroSection/CoreValueCard/style.ts
@@ -0,0 +1,167 @@
+import styled from '@emotion/styled';
+import { colors } from '@sopt-makers/colors';
+import Image from 'next/image';
+import { css } from '@emotion/react';
+
+export const CardContainer = styled.div<{ isActive: boolean }>`
+  position: relative;
+  cursor: pointer;
+  flex-shrink: 0;
+  width: 236px;
+  height: 446px;
+  overflow: hidden;
+  border-radius: 12px;
+  transition: width 0.7s ease;
+
+  @media (min-width: 1024px) {
+    ${({ isActive }) =>
+      isActive &&
+      css`
+        width: 664px;
+      `};
+  }
+
+  @media (max-width: 1023px) and (min-width: 768px) {
+    width: 140px;
+    height: 300px;
+    border-radius: 36px;
+    ${({ isActive }) =>
+      isActive &&
+      css`
+        width: 296px;
+      `};
+  }
+
+  @media (max-width: 767px) {
+    width: 300px;
+    height: 260px;
+    border-radius: 12px;
+    scroll-snap-align: center;
+    scroll-snap-stop: always;
+  }
+`;
+
+export const CoreValueImage = styled(Image)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 12px;
+  object-fit: cover;
+`;
+
+export const GradientOverlay = styled.div<{ isActive: boolean }>`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  z-index: 10;
+  opacity: ${({ isActive }) => isActive && 0.5};
+  transition: opacity 0.3s ease;
+  background: ${({ isActive }) =>
+    isActive
+      ? 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.48) 60.96%)'
+      : 'linear-gradient(0deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0.5) 100%)'};
+`;
+
+export const CardContent = styled.div<{ isActive: boolean }>`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  width: 664px;
+  height: 100%;
+  padding: 36px;
+  z-index: 20;
+  opacity: ${({ isActive }) => (isActive ? 1 : 0)};
+  transition: opacity 0.5s ease;
+
+  @media (max-width: 1023px) and (min-width: 768px) {
+    width: 296px;
+    padding: 24px;
+  }
+
+  @media (max-width: 767px) {
+    width: 100%;
+    padding: 26px;
+  }
+`;
+
+export const CardTitle = styled.h3`
+  font-family: SUIT;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  white-space: pre-line;
+  margin-bottom: 12px;
+
+  @media (max-width: 1023px) and (min-width: 768px) {
+    font-size: 24px;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 19px;
+  }
+`;
+
+// max-width 570px은 데스크탑뷰에서 줄넘기기 깔끔하게 하기 위해 설정. 멘트가 바뀐다면 삭제해도 됨.
+export const CardDetail = styled.p`
+  max-width: 570px;
+
+  color: ${colors.gray100};
+  font-family: SUIT;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 160%;
+  letter-spacing: -0.48px;
+  word-break: keep-all;
+
+  @media (max-width: 1023px) and (min-width: 768px) {
+    font-size: 14px;
+
+  }
+
+  @media (max-width: 767px) {
+    font-size: 14px;
+  }
+`;
+
+export const CardValue = styled.p<{ isActive: boolean }>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-family: SUIT;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 100%;
+  letter-spacing: -0.8px;
+  opacity: ${({ isActive }) => (isActive ? 0 : 1)};
+  transition: opacity 0.5s ease;
+  z-index: 40;
+
+  @media (max-width: 1023px) and (min-width: 768px) {
+    font-size: 24px;
+  }
+`;
+
+export const PrimaryColorCircle = styled.div<{ isActive: boolean }>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(15%, -85%);
+  width: 44px;
+  height: 44px;
+  border-radius: 50%;
+  background-color: #ff5976;
+  z-index: 30;
+  opacity: ${({ isActive }) => (isActive ? 0 : 1)};
+  transition: opacity 0.5s ease;
+
+  @media (max-width: 1023px) and (min-width: 768px) {
+    width: 24px;
+    height: 24px;
+    transform: translate(30%, -105%);
+  }
+`;
--- src/views/MainPage/components/IntroSection/index.tsx
+++ src/views/MainPage/components/IntroSection/index.tsx
@@ -1,17 +1,13 @@
 import { INTRO_CONTENT_LIST } from '@src/lib/constants/main';
-import IntroContent from '@src/views/MainPage/components/IntroSection/IntroContent';
+import CoreValueCardList from '@src/views/MainPage/components/IntroSection/CoreValueCardList';
 import * as S from './style';
 
 export default function IntroCardList() {
   return (
-    <S.IntroSection >
-      <S.Header />
-      <div id="value" style={{ overflow: 'hidden' }}>
-        {INTRO_CONTENT_LIST.map((content) => (
-          <IntroContent key={content.id} content={content} />
-        ))}
-      </div>
-      <S.Footer />
+    <S.IntroSection>
+      <S.CoreValueSection>
+        <CoreValueCardList cards={INTRO_CONTENT_LIST} />
+      </S.CoreValueSection>
     </S.IntroSection>
   );
 }
