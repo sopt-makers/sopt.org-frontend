2706f5e185428708703d940487c4194376a25119
namdaeun <nde40345@gmail.com>
2026-03-06 23:36:58 +0900

    chore: 핵심가치 변경

--- src/lib/constants/main.ts
+++ src/lib/constants/main.ts
@@ -142,27 +142,27 @@ export const Activity: ActivityListType = {
 export const INTRO_CONTENT_LIST = [
   {
     id: 1,
-    title: '열정이 이끄는\n최고 수준의 몰입',
+    title: '포기하지 않고\n끝까지 나아가는 사람',
     detail:
       '열정 하나로 뭉친 솝트는 끊임 없이 집중하며 성장합니다.\n언제나 어떤 일에 대해서든 최고 수준의 몰입을 유지합니다.',
     src: ImgIntroCard1.src,
-    value: '몰입',
+    value: '의지',
   },
   {
     id: 2,
-    title: '가진 것은 무엇이든\n나누는 문화',
+    title: '의지를 생각에서 멈추지 않고\n행동으로 옮기는 사람 ',
     detail:
       '솝트에서는 모두가 자신의 지식과 경험을\n적극적으로 나눕니다. 이를 통해 다양한 관점에서\n세상의 문제를 해결할 수 있습니다.',
     src: ImgIntroCard2.src,
-    value: '공유',
+    value: '실행',
   },
   {
     id: 3,
-    title: '함께이기 때문에\n가능한 도전',
+    title: '함께일 때 더\n성장할 수 있는 사람',
     detail:
       '200여 명의 활동 회원, 3000여 명의 명예 회원들과 함께이기에\n그 어떤 목표에도 용기 내어 도전할 수 있습니다.',
     src: ImgIntroCard3.src,
-    value: '도전',
+    value: '조화',
   },
 ];
 
