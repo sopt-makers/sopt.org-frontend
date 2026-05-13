9a6926d5b21d2749acc1bf1390360797216b4343
namdaeun <nde40345@gmail.com>
2026-05-13 21:44:22 +0900

    feat: add breakpoints and media queries

---
+++ src/lib/styles/breakpoints.ts
@@ -0,0 +1,14 @@
+export const breakpoints = {
+  mobile: '767px',
+  tablet: '1023px',
+  desktop: '1260px',
+  desktopLarge: '2560px',
+} as const;
+
+export type Breakpoint = keyof typeof breakpoints;
+
+export const media = {
+  mobile: `@media (max-width: ${breakpoints.mobile})`,
+  tablet: `@media (max-width: ${breakpoints.tablet})`,
+  desktopLarge: `@media (min-width: ${breakpoints.desktop}) and (max-width: ${breakpoints.desktopLarge})`,
+} as const;
