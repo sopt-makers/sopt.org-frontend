f1fad4493743a5e4320fa00e044efa3beb247b6a
constantly-dev <rntqhdl10@naver.com>
2026-05-18 14:20:55 +0900

    fix: mainColor 추가, build error 해결

--- src/views/AboutPage/components/@common/SectionTop/index.tsx
+++ src/views/AboutPage/components/@common/SectionTop/index.tsx
@@ -1,24 +1,23 @@
+import { useContext } from 'react';
 import { useIsMobile } from '@src/hooks/useDevice';
+import { BrandingColorContext } from '@src/views/AboutPage';
 import * as S from './style';
 
 interface SectionTopProps {
   engTitle?: string;
   korTitle: string;
   description?: string;
-  mainColor: string;
+  mainColor?: string;
 }
 
-export default function SectionTop({
-  engTitle,
-  korTitle,
-  description,
-  mainColor,
-}: SectionTopProps) {
+export default function SectionTop({ engTitle, korTitle, description, mainColor }: SectionTopProps) {
   const isMobile = useIsMobile();
+  const { main } = useContext(BrandingColorContext);
+  const resolvedColor = mainColor ?? '#' + main;
   return (
     <S.SectionTop>
       <S.SectionTitle>
-        <S.EngTitle mainColor={mainColor}>{engTitle}</S.EngTitle>
+        <S.EngTitle mainColor={resolvedColor}>{engTitle}</S.EngTitle>
         <S.KorTitle>{korTitle}</S.KorTitle>
       </S.SectionTitle>
       {description && (
