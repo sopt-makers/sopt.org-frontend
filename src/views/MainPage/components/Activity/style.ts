14fd57b7057a6cfaad0690d8e72b462daa0a3efe
namdaeun <nde40345@gmail.com>
2026-05-21 17:27:31 +0900

    style: 반응형 수정

--- src/views/MainPage/components/Activity/style.ts
+++ src/views/MainPage/components/Activity/style.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { media } from '@src/lib/styles/breakpoints';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -6,4 +7,22 @@ export const Wrapper = styled.div`
   padding-top: 100px;
   gap: 24px;
   overflow: visible;
+
+  ${media.desktop} {
+    width: 100%;
+    align-items: center;
+
+    & > section {
+      width: 100%;
+      max-width: 944px;
+    }
+  }
+
+  ${media.desktopLarge} {
+    align-items: flex-start;
+
+    & > section {
+      max-width: 1200px;
+    }
+  }
 `;
