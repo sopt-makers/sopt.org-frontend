f2d07c4d80b7a4f8f4131ed72d9771a917290f3a
이진혁 <rntqhdl10@naver.com>
2026-05-22 18:45:56 +0900

    [Feat] Project section 구현 (#533)
    * style: Button 컴포넌트 fontsObject 적용 및 반응형 패딩 조정
    
    * feat: Project 카드 active 상태 기반 스타일 및 description ellipsis 처리
    
    * feat: ProjectSection 캐러셀 구현 및 레이아웃 상수 분리
    
    * refactor: Button 컴포넌트 media 상수 사용하도록 변경
    
    * refactor: 리뷰 반영

--- src/views/MainPage/components/IntroSection/ProjectSection/Project/index.tsx
+++ src/views/MainPage/components/IntroSection/ProjectSection/Project/index.tsx
@@ -1,40 +1,35 @@
+import { useRouter } from 'next/router';
 import * as S from './style';
-import icMembers from '@src/assets/icons/ic_members.svg';
 
 interface ProjectProps {
   thumbnail: string;
   title: string;
   category: string;
   description: string;
-  status: string; 
-  members: number;
+  isActive: boolean;
+  href: string;
 }
 
-export default function Project({ thumbnail, title, category, description, status, members }: ProjectProps) {
-  return (
-    <S.Wrapper>
-      <S.Thumbnail src={thumbnail} alt={title} />
+export default function Project({
+  thumbnail,
+  title,
+  category,
+  description,
+  isActive,
+  href,
+}: ProjectProps) {
+  const router = useRouter();
 
+  return (
+    <S.Wrapper $isActive={isActive} onClick={() => router.push(href)}>
+      <S.Thumbnail src={thumbnail} alt={title} $isActive={isActive} />
       <S.ContentWrapper>
         <S.TitleWrapper>
-          <S.Title>{title}</S.Title>
-          <S.Category>{category}</S.Category>
+          <S.Title $isActive={isActive}>{title}</S.Title>
+          <S.Category $isActive={isActive}>{category}</S.Category>
         </S.TitleWrapper>
-        <S.Description>{description}</S.Description>
+        <S.Description $isActive={isActive}>{description}</S.Description>
       </S.ContentWrapper>
-      
-      <S.FooterLayout> 
-        {status !== '' && (
-        <S.StatusLayout>
-          <S.StatusCircle />
-          <S.Status>{status}</S.Status>
-        </S.StatusLayout> 
-        )}
-        <S.Members>
-          <S.MembersIcon src={icMembers} alt="members" aria-hidden="true" />
-          {members} members
-        </S.Members>
-      </S.FooterLayout>
     </S.Wrapper>
   );
 }
