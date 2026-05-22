b84f5df38c115ab39491b2b099c2f4ad2ff07733
Daeun Nam <96781926+namdaeun@users.noreply.github.com>
2026-05-22 17:11:20 +0900

    [Feat] Internal Teams 섹션 구현 (#527)
    * feat: add breakpoints and media queries
    
    * refactor: update Select component to use optional breakpoints
    
    * fix: Breakpoint 타입을 media 키 기준으로 수정
    
    * feat: apply branding main color to SectionTop engTitle
    
    * feat: EngTitle color 없을 때 default 지정
    
    * feat: InternalTeams 섹션 구현
    
    * style: modify page layout
    
    * chore: add images
    
    * feat: add internal teams component
    
    * style: 반응형 추가
    
    * chore: 폴더명 변경

--- src/views/AboutPage/components/Curriculum/Section/style.ts
+++ src/views/AboutPage/components/Curriculum/Section/style.ts
@@ -5,17 +5,14 @@ export const CurriculumSection = styled.section`
   flex-direction: column;
   align-items: center;
   gap: 48px;
-  margin-top: 275px;
 
   /* 태블릿 뷰 */
   @media (max-width: 48rem) {
     gap: 19px;
-    margin-top: 190px;
   }
 
   /* 모바일 뷰 */
   @media (max-width: 26.75rem) {
     gap: 13.44px;
-    margin-top: 120px;
   }
 `;
