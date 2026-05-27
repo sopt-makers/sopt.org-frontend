a797c079ea515ed521d587a7a12dfa937e6f3213
namdaeun <nde40345@gmail.com>
2026-05-28 00:00:45 +0900

    refactor: useGeneration이 useHomepage를 재사용하도록 변경

--- src/hooks/useGeneration.ts
+++ src/hooks/useGeneration.ts
@@ -1,14 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
-import { remoteAdminAPI } from '@src/lib/api/remote/admin';
-import { GetHomepageResponse } from '@src/lib/types/admin';
-import { HOMEPAGE_QUERY_KEY } from '@src/views/MainPage/hooks/useHomepage';
+import { useHomepage } from '@src/views/MainPage/hooks/useHomepage';
 
 /** 현재 SOPT 기수만 가져옴 */
 export const useGeneration = (): number | null => {
-  const { data } = useQuery<GetHomepageResponse>({
-    queryKey: HOMEPAGE_QUERY_KEY,
-    queryFn: remoteAdminAPI.getHomepage,
-  });
+  const { data } = useHomepage();
 
   return data?.generation ?? null;
 };
--- src/views/MainPage/hooks/useHomepage.ts
+++ src/views/MainPage/hooks/useHomepage.ts
@@ -2,11 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import { remoteAdminAPI } from '@src/lib/api/remote/admin';
 import { GetHomepageResponse } from '@src/lib/types/admin';
 
-export const HOMEPAGE_QUERY_KEY = ['homepage'] as const;
-
 export const useHomepage = () => {
   return useQuery<GetHomepageResponse>({
-    queryKey: HOMEPAGE_QUERY_KEY,
+    queryKey: ['homepage'],
     queryFn: remoteAdminAPI.getHomepage,
   });
 };
