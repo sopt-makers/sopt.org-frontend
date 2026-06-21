b374a9d127d31291412e4c5212360cb9b625238a
namdaeun <nde40345@gmail.com>
2026-06-21 21:23:30 +0900

    fix: 모집안내 버튼 지원서 url로 라우팅 되도록 수정

--- src/views/MainPage/components/@common/RecruitButton/index.tsx
+++ src/views/MainPage/components/@common/RecruitButton/index.tsx
@@ -1,6 +1,6 @@
 import router from 'next/router';
 import { ButtonHTMLAttributes, PropsWithChildren, useState } from 'react';
-import { PATHS } from '@src/lib/constants/routes';
+import { RECRUIT_URL } from '@src/lib/constants/routes';
 import * as S from './style';
 
 export type RecruitButtonSize = 'sm' | 'md';
@@ -21,7 +21,7 @@ export default function RecruitButton({
   const [blurPosition, setBlurPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
 
   const handleClick = () => {
-    router.push(PATHS.RECRUIT);
+    router.push(RECRUIT_URL);
   };
 
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
