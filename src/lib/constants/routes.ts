f5e9b2d5eac6566f6629775e2ee7fa2f12655efd
이진혁 <rntqhdl10@naver.com>
2026-08-02 14:11:19 +0900

    Merge pull request #567 from sopt-makers/feat/#566_add-privacy-policy-notion-link
    [Feat] 개인정보처리방침 추가 (update 26.08.02)

--- src/lib/constants/routes.ts
+++ src/lib/constants/routes.ts
@@ -9,3 +9,4 @@ export const PATHS = {
 } as const;
 
 export const RECRUIT_URL = 'https://recruit.sopt.org';
+export const PRIVACY_POLICY_URL = 'https://sopt-makers.notion.site/d17c0071ab0e440baadd36c548bc36e4';
