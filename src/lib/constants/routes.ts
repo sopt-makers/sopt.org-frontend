ec22dac4a8d83d1a81dad1292f47a56ced0674e8
namdaeun <nde40345@gmail.com>
2026-08-29 14:11:04 +0900

    hotfix: /recruit 경로 대신 지원서 서비스로 이동

--- src/lib/constants/routes.ts
+++ src/lib/constants/routes.ts
@@ -4,9 +4,9 @@ export const PATHS = {
   PROJECT: '/project',
   BLOG: '/blog',
   SPONSOR: '/sponsor',
-  RECRUIT: '/recruit',
   RULES: '/rules',
 } as const;
 
 export const RECRUIT_URL = 'https://recruit.sopt.org';
-export const PRIVACY_POLICY_URL = 'https://sopt-makers.notion.site/d17c0071ab0e440baadd36c548bc36e4';
+export const PRIVACY_POLICY_URL =
+  'https://sopt-makers.notion.site/d17c0071ab0e440baadd36c548bc36e4';
